744be0b356c1d5fcde22eb26f2c6537b6a72b224
萌新杰少 <1250422131@qq.com>
2023-02-06 21:59:25 +0800

    1.新增追番记录导出excel功能（初步实现，存在效率问题） 1. Added the function of tracking record export excel (preliminary implementation, there is an efficiency problem)
    Former-commit-id: aa5bea12c3b3a9fc4fe26219fc3232801298b13e

--- tool_roam/src/main/java/com/imcys/bilibilias/tool_roam/model/RoamItemBean.kt
+++
@@ -1,8 +0,0 @@
-package com.imcys.bilibilias.tool_roam.model
-
-import com.imcys.bilibilias.common.data.entity.RoamInfo
-
-data class RoamItemBean(
-    val tag :Int,
-    val roamInfo: RoamInfo?
-)
