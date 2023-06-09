00fd493b55186310169d4d8adbbd43e5c2cd172b
cys <1250422131@qq.com>
2023-06-09 15:25:18 +0800

    1.实验性提交（修改下载引擎改okDownload）尝试实现多线程。 2.支持竖屏视频全屏播放时使用额外全屏方案（小窗将在下个版本实验）

--- app/src/main/java/com/imcys/bilibilias/home/ui/activity/DonateActivity.kt
+++ app/src/main/java/com/imcys/bilibilias/home/ui/activity/DonateActivity.kt
@@ -15,10 +15,13 @@ import com.imcys.bilibilias.home.ui.model.OldDonateBean
 import com.imcys.bilibilias.home.ui.model.TipBean
 import com.imcys.bilibilias.common.base.utils.http.HttpUtils
 import com.zackratos.ultimatebarx.ultimatebarx.addStatusBarTopPadding
+import javax.inject.Inject
 
 class DonateActivity : BaseActivity() {
     private val donateMutableList = mutableListOf<DonateViewBean>()
     lateinit var binding: ActivityDonateBinding
+
+    @Inject
     lateinit var donateAdapter: DonateItemAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +41,6 @@ class DonateActivity : BaseActivity() {
 
     private fun initRv() {
 
-        donateAdapter = DonateItemAdapter()
         binding.apply {
             donateRv.adapter = donateAdapter
             donateRv.layoutManager =
@@ -75,7 +77,7 @@ class DonateActivity : BaseActivity() {
                 )
             )
 
-            donateAdapter.submitList(donateMutableList +  newMutableList)
+            donateAdapter.submitList(donateMutableList + newMutableList)
         }
     }
 
