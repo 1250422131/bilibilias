4f6944b6d51cf97884987ce11b5d28fa15625f5a
xiwangly2 <1334850101@qq.om>
2023-06-18 22:31:09 +0800

    测试修改语言（成功）

--- app/src/main/java/com/imcys/bilibilias/base/app/App.kt
+++ app/src/main/java/com/imcys/bilibilias/base/app/App.kt
@@ -2,11 +2,8 @@ package com.imcys.bilibilias.base.app
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.SharedPreferences
-import android.os.Handler
 import com.drake.brv.utils.BRV
 import com.drake.statelayout.StateConfig
-import com.imcys.bilibilias.BuildConfig
 import com.imcys.bilibilias.R
 import com.imcys.bilibilias.base.utils.DownloadQueue
 import com.imcys.bilibilias.common.base.app.BaseApplication
