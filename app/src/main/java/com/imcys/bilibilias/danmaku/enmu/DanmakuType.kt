c13a40da5695a9b398d693414d7aa960bd92cf3d
cys <1250422131@qq.com>
2023-07-20 22:30:12 +0800

    新增XML转ASS弹幕测试功能 移除直播模块
    Former-commit-id: e8e5ba5d8fabb737ed877bba4529b034a8ac3a2d

---
+++ app/src/main/java/com/imcys/bilibilias/danmaku/enmu/DanmakuType.kt
@@ -0,0 +1,11 @@
+package com.imcys.bilibilias.danmaku.enmu
+
+/**
+ * 弹幕类型
+ */
+enum class DanmakuType {
+    OrdinaryDanmaku, // 普通弹幕
+    TopDanmaku, //顶部弹幕
+    BottomDanmaku, //底部弹幕
+    ReverseDanmaku,//逆向弹幕
+}
