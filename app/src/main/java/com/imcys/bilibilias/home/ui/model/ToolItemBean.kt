e8e5ba5d8fabb737ed877bba4529b034a8ac3a2d
cys <1250422131@qq.com>
2023-07-20 22:30:12 +0800

    新增XML转ASS弹幕测试功能 移除直播模块

--- app/src/main/java/com/imcys/bilibilias/home/ui/model/ToolItemBean.kt
+++ app/src/main/java/com/imcys/bilibilias/home/ui/model/ToolItemBean.kt
@@ -1,6 +1,5 @@
 package com.imcys.bilibilias.home.ui.model
 
-import com.imcys.bilibilias.tool_livestream.ui.model.LiveRoomDataBean
 
 data class ToolItemBean(
 
@@ -10,7 +9,6 @@ data class ToolItemBean(
     //0为普通工具，1为视频，番剧，2为直播
     val type: Int = 0,
     val videoBaseBean: VideoBaseBean? = null,
-    val liveRoomDataBean: LiveRoomDataBean? = null,
     val clickEvent: () -> Unit = {},
 
     )
