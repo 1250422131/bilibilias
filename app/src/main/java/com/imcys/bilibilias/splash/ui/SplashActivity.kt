c4fccdf121502d450c70530276d67329d8babb86
cys <1250422131@qq.com>
2023-07-29 17:09:16 +0800

    修复部分用户因为uid长度导致登录闪退的问题 调整API接口类直接为object类 更新混淆类的规则
    Former-commit-id: 04ba66771e4c2e56ed0729993af0c6dffe1a6191

--- app/src/main/java/com/imcys/bilibilias/splash/ui/SplashActivity.kt
+++ app/src/main/java/com/imcys/bilibilias/splash/ui/SplashActivity.kt
@@ -2,16 +2,12 @@ package com.imcys.bilibilias.splash.ui
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.os.Handler
 import android.os.Looper
-import android.provider.Settings
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,10 +17,8 @@ import com.imcys.bilibilias.R
 import com.imcys.bilibilias.base.BaseActivity
 import com.imcys.bilibilias.base.utils.DialogUtils
 import com.imcys.bilibilias.home.ui.activity.HomeActivity
-import com.liulishuo.okdownload.OkDownloadProvider.context
 import com.tencent.mmkv.MMKV
 import com.zackratos.ultimatebarx.ultimatebarx.addStatusBarTopPadding
-
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
@@ -38,42 +32,38 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        getSavePermissions()
+        // 首先检查是否已经授予了储存权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // 安卓13废弃对写入权限检测
+            // TODO 将准备改为SAF，届时不在对软件检查储存权限
+            toHome()
+        } else {
+            getSavePermissions()
+        }
         val constraintLayout = findViewById<ConstraintLayout>(R.id.splash_top)
         constraintLayout.addStatusBarTopPadding()
-
-
     }
 
     override fun onBackPressed() {
         moveTaskToBack(true)
     }
 
-
     override fun onDestroy() {
         super.onDestroy()
         delayedHandler?.removeCallbacksAndMessages(null)
-
     }
 
-
     private fun getSavePermissions() {
-        // 首先检查是否已经授予了储存权限
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            //安卓13废弃对写入权限检测
-            //TODO 将准备改为SAF，届时不在对软件检查储存权限
-            toHome()
-        }
         if (ContextCompat.checkSelfPermission(
                 this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
             )
             != PackageManager.PERMISSION_GRANTED
         ) {
             // 如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true
             if (ActivityCompat.shouldShowRequestPermissionRationale(
                     this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 )
             ) {
                 // 在这里可以弹出一个对话框来说明为什么需要此权限
@@ -88,13 +78,14 @@ class SplashActivity : BaseActivity() {
                         ActivityCompat.requestPermissions(
                             this,
                             arrayOf(
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE
-                            ), REQUEST_CODE_WRITE_EXTERNAL_STORAGE
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            ),
+                            REQUEST_CODE_WRITE_EXTERNAL_STORAGE,
                         )
                     },
                     negativeButtonClickListener = {
                         finishAll()
-                    }
+                    },
                 ).show()
             } else {
                 // 申请储存权限
@@ -109,22 +100,20 @@ class SplashActivity : BaseActivity() {
                         ActivityCompat.requestPermissions(
                             this,
                             arrayOf(
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE
-                            ), REQUEST_CODE_WRITE_EXTERNAL_STORAGE
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            ),
+                            REQUEST_CODE_WRITE_EXTERNAL_STORAGE,
                         )
                     },
                     negativeButtonClickListener = {
                         // 处理取消按钮点击事件
                         finishAll()
-                    }
+                    },
                 ).show()
-
-
             }
         } else {
             toHome()
         }
-
     }
 
     private fun toHome() {
@@ -133,10 +122,8 @@ class SplashActivity : BaseActivity() {
             isFirstLoaded = true
             delayedHandler = Handler(Looper.getMainLooper())
             delayedHandler?.let {
-
-                //迁移旧的数据
+                // 迁移旧的数据
                 initMMVKData()
-
 
                 HandlerCompat.postDelayed(it, {
                     // 创建一个意图，说明我要跳转到那个活动界面。
@@ -146,14 +133,12 @@ class SplashActivity : BaseActivity() {
                     // 再来个跳转过度动画。
                     overridePendingTransition(
                         android.R.anim.fade_in,
-                        android.R.anim.fade_out
+                        android.R.anim.fade_out,
                     )
                     // 销毁当前活动。
                     finish()
-
                 }, null, 1000)
             }
-
         }
     }
 
@@ -161,16 +146,12 @@ class SplashActivity : BaseActivity() {
      * 如果存在旧数据，意味着是旧版本的升级用户，这里进行数据迁移
      */
     private fun initMMVKData() {
-
         getSharedPreferences("data", MODE_PRIVATE).apply {
-
             if (!getString("cookies", "").equals("")) {
                 MMKV.mmkvWithID("data")!!.importFromSharedPreferences(this)
                 this.edit { clear() }
             }
-
         }
-
     }
 
     override fun onRequestPermissionsResult(
