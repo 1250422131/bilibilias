c032a87fe8f9dc56f2fc836e360246a2594dc2dd
sock_magic <startxingcheng@gmail.com>
2023-08-24 23:12:32 +0800

    chore: lib迁移基本完成
    Former-commit-id: 61f92722c4239495c64e4cb353fe85c5e14712a4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version ("7.4.2") apply false
-    id("com.android.library") version ("7.4.2") apply false
-    id("org.jetbrains.kotlin.android") version ("1.8.10") apply false
-    id("com.google.devtools.ksp") version ("1.8.10-1.0.9") apply false
-    id("com.google.dagger.hilt.android") version ("2.44") apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
 
-tasks.register("clean") {
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
