41f68983b21e110a78605ea07126158c37722e0d
sock_magic <startxingcheng@gmail.com>
2023-08-27 09:10:50 +0800

    refactor: StringBuffer替换为StringBuilder
    Former-commit-id: e7cf620fee970e63b682ff23d7d3b78203c98640

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/file/SystemUtil.java
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/file/SystemUtil.java
@@ -91,7 +91,7 @@ public class SystemUtil {
         } catch (Exception e) {
             userAgent = System.getProperty("http.agent");
         }
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0, length = userAgent.length(); i < length; i++) {
             char c = userAgent.charAt(i);
             if (c <= '\u001f' || c >= '\u007f') {
