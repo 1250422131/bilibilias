ff6a86dd0b9f36af11467beb9abd1378c5d5a6aa
sock_magic <startxingcheng@gmail.com>
2023-08-28 13:27:40 +0800

    fix(develop): 修复脚本迁移遗漏

--- model_ffmpeg/build.gradle.kts
+++ model_ffmpeg/build.gradle.kts
@@ -1,6 +1,7 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin)
 }
 
 android {
@@ -43,10 +44,10 @@ kotlin {
 
 dependencies {
     implementation(project(":common"))
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
