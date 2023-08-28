ce4e8531a613476d5f6cd5cf9e01eef58a66c262
sock_magic <startxingcheng@gmail.com>
2023-08-28 17:05:43 +0800

    refactor(develop): asUser放到BaseApplication

--- common/src/main/java/com/imcys/bilibilias/common/base/app/BaseApplication.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/app/BaseApplication.kt
@@ -6,8 +6,9 @@ import android.os.Handler
 import androidx.preference.PreferenceManager
 import com.baidu.mobstat.StatService
 import com.imcys.bilibilias.common.BuildConfig
+import com.imcys.bilibilias.common.base.constant.COOKIES
+import com.imcys.bilibilias.common.base.model.user.AsUser
 import com.imcys.bilibilias.common.base.model.user.MyUserData
-import com.liulishuo.okdownload.OkDownloadProvider.context
 import com.tencent.mmkv.MMKV
 import com.xiaojinzi.component.Component
 import com.xiaojinzi.component.Config
@@ -24,14 +25,13 @@ open class BaseApplication : Application() {
 
         handler = Handler(mainLooper)
 
-        //百度统计开始
+        // 百度统计开始
         startBaiDuService()
 
         initKComponent()
 
-        //初始化MMKV
+        // 初始化MMKV
         initMMKV()
-
     }
 
     private fun initMMKV() {
@@ -48,10 +48,12 @@ open class BaseApplication : Application() {
         )
         // 手动加载模块
         ModuleManager.registerArr(
-            "app", "common", "tool_livestream", "tool_log_export"
+            "app",
+            "common",
+            "tool_livestream",
+            "tool_log_export"
         )
     }
-
 
     /**
      * 百度统计
@@ -63,29 +65,37 @@ open class BaseApplication : Application() {
         StatService.autoTrace(applicationContext)
     }
 
-
     companion object {
 
         const val appSecret = "3c7c5174-a6be-4093-a0df-c6fbf7371480"
         const val AppGuideVersion = "1.0"
 
-
-        //全局应用数据的MMKV
+        // 全局应用数据的MMKV
         lateinit var dataKv: MMKV
             private set
+        val asUser: AsUser
+            get() = run {
+                val kv = BaseApplication.dataKv
+                AsUser.apply {
+                    cookie = kv.decodeString(COOKIES, "")!!
+                    sessdata = kv.decodeString("SESSDATA", "")!!
+                    biliJct = kv.decodeString("bili_jct", "")!!
+                    mid = kv.decodeLong("mid", 0)
+                    asCookie = kv.decodeString("as_cookie", "")!!
+                }
+            }
 
-        //——————————————————全局线程处理器——————————————————
+        // ——————————————————全局线程处理器——————————————————
         lateinit var handler: Handler
             private set
-        //—————————————————————————————————————————————————
+        // —————————————————————————————————————————————————
 
-        //——————————————————B站视频模板——————————————————
+        // ——————————————————B站视频模板——————————————————
 
-
-
-        //——————————————————部分内置需要的上下文——————————————————
+        // ——————————————————部分内置需要的上下文——————————————————
 
         private var instance: BaseApplication? = null
+
         @JvmStatic
         fun applicationContext(): Context {
             return instance!!.applicationContext
@@ -93,8 +103,6 @@ open class BaseApplication : Application() {
 
         var mid: Long = 0
         lateinit var myUserData: MyUserData.DataBean
-        //—————————————————————————————————————————————————
-
-
+        // —————————————————————————————————————————————————
     }
 }
