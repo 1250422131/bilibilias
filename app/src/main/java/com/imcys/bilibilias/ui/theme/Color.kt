b67eded666e7bd2b9e0f5a2a33acc8d62ca78c4d
sock_magic <startxingcheng@gmail.com>
2023-08-30 12:35:01 +0800

    feat(dev_compose): 主题，颜色添加
    Former-commit-id: c95efa2c4179babec41987f703024bccafa8b1bd

---
+++ app/src/main/java/com/imcys/bilibilias/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.imcys.bilibilias.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val color_primary_back = Color(0XFF0b3DFB)
+val color_primary_text_back = Color(0XFF0b3DFB)
+val color_primary_variant_back = Color(0XFFE2E6FA)
+
+val color_primary = Color(0XFFfb7299)
+val color_primary_text = Color(0XFFfb7299)
+val color_primary_variant = Color(0XFFffd2e0)
+val color_primary_two = Color(0XFF30CFB1)
+val color_primary_light_text = Color(0XFF05080C)
+
+val purple_200 = Color(0XFFBB86FC)
+val purple_500 = Color(0XFF6200EE)
+val purple_700 = Color(0XFF3700B3)
+val teal_200 = Color(0XFFffd2e0)
+val teal_700 = Color(0XFFfb7299)
+val black = Color(0XFF000000)
+val white = Color(0XFFFFFFFF)
+val color_text_hint = Color(0XFFA6A6AD)
+val color_121212 = Color(0XFFFFFFFF)
+val color_303030 = Color(0XFFFFFFFF)
+val user_work_bg = Color(0XFFF7F8FA)
+val app_default_bg = Color(0XFFf2f2f2)
