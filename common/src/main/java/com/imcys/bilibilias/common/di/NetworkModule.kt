420c1d572603e0f7ee27d9610b81ce09b8dba028
sock_magic <startxingcheng@gmail.com>
2023-09-01 22:25:33 +0800

    feat(dev_compose): 转换数据插件，但是还不完善
    Former-commit-id: 8f54a7be8d44f1226aef253c42ab1f3c43a97d1e

--- common/src/main/java/com/imcys/bilibilias/common/di/NetworkModule.kt
+++ common/src/main/java/com/imcys/bilibilias/common/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.imcys.bilibilias.common.di
 
+import android.content.Context
 import com.imcys.bilibilias.base.utils.asLogD
 import com.imcys.bilibilias.common.base.api.BiliBiliAsApi
 import com.imcys.bilibilias.common.base.app.BaseApplication
@@ -9,46 +10,56 @@ import com.imcys.bilibilias.common.base.utils.file.SystemUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import github.leavesczy.monitor.MonitorInterceptor
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.BrowserUserAgent
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.plugins.plugin
 import io.ktor.client.request.header
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.userAgent
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.intOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.serializer
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+
     @Provides
     @Singleton
-    fun provideHttpClient(): HttpClient = HttpClient(
+    fun provideHttpClient(@ApplicationContext appContext: Context): HttpClient = HttpClient(
         OkHttp.create {
-            addInterceptor { chain ->
-                chain.proceed(chain.request())
-            }
+            addInterceptor(MonitorInterceptor(appContext))
         }
     ) {
         defaultRequest {
             url(BILIBILI_URL)
         }
         BrowserUserAgent()
+        install(HttpCookies)
+        install(convertPlugin)
         install(DefaultRequest) {
             header(HttpHeaders.ContentType, ContentType.Application.Json)
         }
@@ -71,7 +82,11 @@ class NetworkModule {
             exponentialDelay()
         }
         install(Logging) {
-            logger = Logger.DEFAULT
+            logger = object : Logger {
+                override fun log(message: String) {
+                    asLogD("Plugin", message)
+                }
+            }
             level = LogLevel.ALL
             filter { request ->
                 request.url.host.contains("ktor.io")
@@ -96,4 +111,19 @@ class NetworkModule {
             }
         }
     }
+
+    @OptIn(InternalSerializationApi::class)
+    private val convertPlugin =
+        createClientPlugin("ConvertPlugin") {
+            transformResponseBody { response, content, requestedType ->
+                val jsonObject = Json.parseToJsonElement(response.bodyAsText()).jsonObject
+                val code = jsonObject["code"]?.jsonPrimitive?.intOrNull
+                if (code == 0) {
+                    val jsonElement = jsonObject["data"]
+                    val obj = jsonElement?.jsonObject
+                }
+                val type = requestedType.type.serializer()
+                Json.decodeFromJsonElement(type, jsonObject["data"]!!)
+            }
+        }
 }
