5b6d1cf01a70535a9c7b942ec0c698dbe67be3c3
sock_magic <startxingcheng@gmail.com>
2023-09-01 22:04:27 +0800

    feat(dev_compose): 路由添加

--- app/src/main/java/com/imcys/bilibilias/base/router/SplashRouter.kt
+++ app/src/main/java/com/imcys/bilibilias/base/router/SplashRouter.kt
@@ -3,4 +3,6 @@ package com.imcys.bilibilias.base.router
 sealed class SplashRouter(val route: String) {
     data object App : SplashRouter("app")
     data object Screen : SplashRouter("home")
+    data object AuthMethod : SplashRouter("auth1")
+    data object AuthScreen : SplashRouter("auth2")
 }
