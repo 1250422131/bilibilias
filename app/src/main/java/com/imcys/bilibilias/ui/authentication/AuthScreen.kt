630464f38ca5742a338e92cbf67533877bf069cc
sock_magic <startxingcheng@gmail.com>
2023-09-07 12:22:49 +0800

    feat(dev_compose): 登录
    Former-commit-id: 7063e19a4916560f548dd8a8847e3eec54879d00

--- app/src/main/java/com/imcys/bilibilias/ui/authentication/AuthScreen.kt
+++ app/src/main/java/com/imcys/bilibilias/ui/authentication/AuthScreen.kt
@@ -1,5 +1,6 @@
 package com.imcys.bilibilias.ui.authentication
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +10,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,19 +33,23 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil.compose.SubcomposeAsyncImage
 import com.imcys.bilibilias.R
 import com.imcys.bilibilias.base.router.SplashRouter
-import com.imcys.bilibilias.common.base.api.BilibiliApi
 import com.imcys.bilibilias.common.base.components.FullScreenScaffold
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    val viewModel = hiltViewModel<AuthViewModel>()
+    val authViewModel = hiltViewModel<AuthViewModel>()
+    val authStateState by authViewModel.authState.collectAsState()
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
     FullScreenScaffold(topBar = {
         CenterAlignedTopAppBar(title = {
             Text(
@@ -43,6 +58,16 @@ fun AuthScreen(navController: NavHostController, modifier: Modifier = Modifier) 
                 fontWeight = FontWeight.Bold
             )
         })
+    }, snackbarHost = {
+        LaunchedEffect(authStateState.snackBarMessage) {
+            val message = authStateState.snackBarMessage
+            if (message.isNotBlank()) {
+                snackbarHostState.showSnackbar(authStateState.snackBarMessage)
+            }
+        }
+        SnackbarHost(snackbarHostState) { data ->
+            Snackbar { Text(data.visuals.message) }
+        }
     }) { innerPadding ->
         Column(
             modifier
@@ -50,21 +75,17 @@ fun AuthScreen(navController: NavHostController, modifier: Modifier = Modifier) 
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(BilibiliApi.getLoginQRPath)
-                    .listener(
-                        onStart = {},
-                        onCancel = {},
-                        onError = { request, result ->
-
-                        },
-                    ) { request, result ->
-
-                    }
-                    .build(),
+            var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+            SubcomposeAsyncImage(
+                model = authStateState.qrCodeUrl,
+                loading = {
+                    CircularProgressIndicator()
+                },
+                onSuccess = {
+                    bitmap = it.result.drawable.toBitmap()
+                },
                 contentDescription = "qr code",
-                contentScale = ContentScale.Inside,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.size(130.dp)
             )
             Text(
@@ -73,57 +94,87 @@ fun AuthScreen(navController: NavHostController, modifier: Modifier = Modifier) 
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.primary
             )
-            Button(
-                onClick = { navController.navigate(SplashRouter.Screen.route) },
-                Modifier
-                    .padding(horizontal = 25.dp, vertical = 10.dp)
-                    .height(60.dp)
-                    .fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = Color.White
-                )
-            ) {
-                Text(
-                    text = stringResource(R.string.app_dialog_login_qr_bottomsheet_finish),
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Bold
-                )
+            CompleteLoginButton {
+                authViewModel.completeSigning()
+                navController.navigate(SplashRouter.Screen.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        inclusive = true
+                    }
+                }
             }
-            Button(
-                onClick = { },
-                Modifier
-                    .padding(horizontal = 25.dp, vertical = 10.dp)
-                    .height(60.dp)
-                    .fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(
-                    text = stringResource(R.string.app_dialog_login_qr_bottomsheet_download),
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Bold
-                )
+            val photoName = stringResource(R.string.app_LoginQRModel_downloadLoginQR_fileName)
+            DownloadQrCodeButton {
+                if (bitmap != null) {
+                    authViewModel.downloadQrCode(bitmap!!, photoName, context)
+                }
+                bitmap = null
             }
-            Button(
-                onClick = { },
-                Modifier
-                    .padding(horizontal = 25.dp, vertical = 10.dp)
-                    .height(60.dp)
-                    .fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(
-                    text = stringResource(R.string.app_dialog_login_qr_bottomsheet_go),
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Bold
-                )
+            JumpScanCodeButton {
+                authViewModel.goToBilibiliQrScan(context)
             }
         }
+    }
+}
+
+@Composable
+private fun JumpScanCodeButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        Modifier
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .height(60.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.Black
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.app_dialog_login_qr_bottomsheet_go),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun DownloadQrCodeButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        Modifier
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .height(60.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.Black
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.app_dialog_login_qr_bottomsheet_download),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun CompleteLoginButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        Modifier
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .height(60.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.app_dialog_login_qr_bottomsheet_finish),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
