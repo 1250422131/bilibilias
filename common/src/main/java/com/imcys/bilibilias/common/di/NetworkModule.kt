064d51b5484319262ce34f7d05952b53a23f84b5
sock_magic <startxingcheng@gmail.com>
2023-09-07 12:18:00 +0800

    feat(dev_compose): 日志及cookie

--- common/src/main/java/com/imcys/bilibilias/common/di/NetworkModule.kt
+++ common/src/main/java/com/imcys/bilibilias/common/di/NetworkModule.kt
@@ -3,9 +3,7 @@ package com.imcys.bilibilias.common.di
 import android.content.Context
 import com.imcys.bilibilias.base.utils.asLogD
 import com.imcys.bilibilias.common.base.api.BiliBiliAsApi
-import com.imcys.bilibilias.common.base.app.BaseApplication
-import com.imcys.bilibilias.common.base.constant.BILIBILI_URL
-import com.imcys.bilibilias.common.base.constant.COOKIE
+import com.imcys.bilibilias.common.base.constant.ROAM_API
 import com.imcys.bilibilias.common.base.utils.file.SystemUtil
 import dagger.Module
 import dagger.Provides
@@ -24,7 +22,6 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.plugins.plugin
@@ -32,7 +29,6 @@ import io.ktor.client.request.header
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
 import io.ktor.http.userAgent
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.utils.io.errors.IOException
@@ -43,6 +39,7 @@ import kotlinx.serialization.json.intOrNull
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.serializer
+import okhttp3.CookieJar
 import javax.inject.Singleton
 
 
@@ -55,13 +52,18 @@ class NetworkModule {
     fun provideHttpClient(@ApplicationContext appContext: Context): HttpClient = HttpClient(
         OkHttp.create {
             addInterceptor(MonitorInterceptor(appContext))
+            config {
+                cookieJar(CookieJar.NO_COOKIES)
+            }
         }
     ) {
         defaultRequest {
-            url(BILIBILI_URL)
+            url(ROAM_API)
         }
         BrowserUserAgent()
-        install(HttpCookies)
+        install(HttpCookies) {
+            storage = CookieManager()
+        }
         install(convertPlugin)
         install(DefaultRequest) {
             header(HttpHeaders.ContentType, ContentType.Application.Json)
@@ -85,25 +87,13 @@ class NetworkModule {
             exponentialDelay()
         }
         install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    asLogD("Plugin", message)
-                }
-            }
+            logger = LoggerManager()
             level = LogLevel.ALL
-            filter { request ->
-                request.url.host.contains("ktor.io")
-            }
-            sanitizeHeader { header -> header == HttpHeaders.Authorization }
         }
     }.apply {
         plugin(HttpSend).intercept { request ->
             if (request.headers.contains("misakamoe")) {
                 request.userAgent(SystemUtil.getUserAgent() + " BILIBILIAS/${BiliBiliAsApi.version}")
-            }
-            // todo 注意是否还有其他方法使用cookie
-            if (request.method == HttpMethod.Get) {
-                request.header(COOKIE, BaseApplication.asUser.cookie)
             }
 
             val originalCall = execute(request)
@@ -133,6 +123,7 @@ class NetworkModule {
                     val type = requestedType.type
                     Json.decodeFromJsonElement(type.serializer(), data)
                 } catch (e: Exception) {
+                    // todo {"code":-101,"message":"账号未登录","ttl":1}
                     throw IOException(e.localizedMessage)
                 }
             }
