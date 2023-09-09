471d022afed18719f5e74182d001c3681d08e630
sock_magic <startxingcheng@gmail.com>
2023-09-09 17:24:13 +0800

    build(dev_compose): 升级
    Former-commit-id: b090707923a304319ba97e0e1dd87bae58662f6a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://jitpack.io")
         mavenCentral()
         google()
-        maven("https://jitpack.io")
-        maven("https://maven.aliyun.com/repository/public")
 
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/jcenter")
