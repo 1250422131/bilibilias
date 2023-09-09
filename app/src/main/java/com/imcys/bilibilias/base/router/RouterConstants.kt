7e30892bd7657649d1c779578f27a3f7e5cb5723
sock_magic <startxingcheng@gmail.com>
2023-09-09 16:04:14 +0800

    refactor(dev_compose): 路由常量替换
    Former-commit-id: bc70ae29eabb739532d9e9fce637bb531d45db87

---
+++ app/src/main/java/com/imcys/bilibilias/base/router/RouterConstants.kt
@@ -0,0 +1,12 @@
+package com.imcys.bilibilias.base.router
+
+object RouterConstants {
+    const val Home = "home"
+    const val Tool = "tool"
+    const val Download = "download"
+    const val User = "user"
+
+    const val App = "app"
+    const val Auth1 = "auth1"
+    const val Auth2 = "auth2"
+}
