6ef705819f66e59a3777b768de13fc343ed86ad3
sock_magic <startxingcheng@gmail.com>
2023-09-13 09:58:36 +0800

    refactor(dev_compose): java to kotlin
    Former-commit-id: 2b2b339b617402d21a585a4c47538f62d8ce47a1

--- common/src/main/java/com/imcys/bilibilias/common/base/model/OldHomeBannerDataBean.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/model/OldHomeBannerDataBean.kt
@@ -1,15 +1,13 @@
-package com.imcys.bilibilias.home.ui.model;
+package com.imcys.bilibilias.common.base.model
 
-import java.io.Serializable;
-import java.util.List;
+import java.io.Serializable
 
 /**
  * @author:imcys
  * @create: 2023-01-01 18:05
  * @Description: 旧版首页轮播图数据
  */
-public class OldHomeBannerDataBean implements Serializable {
-
+class OldHomeBannerDataBean : Serializable {
     /**
      * code : 0
      * imgUrlList : ["https://i.328888.xyz/img/2022/11/30/ikl2C.jpeg","http://message.biliimg.com/bfs/im/500ac4d77ef89bcfc3ae503ec3a8bb72a1a483fa.png","https://i.loli.net/2021/04/03/mTuUekRxQ5HrFIB.png","https://i.loli.net/2021/02/23/ErYSKClUhatXxVR.png","https://s3.ax1x.com/2021/01/16/sBYWkD.png","https://i.loli.net/2021/06/13/ShOYsRWHAeFD5iz.png"]
@@ -22,95 +20,14 @@ public class OldHomeBannerDataBean implements Serializable {
      * token : ["",null,null,null,null,null]
      * time : 7000
      */
-
-    private int code;
-    private int time;
-    private List<String> imgUrlList;
-    private List<String> textList;
-    private List<String> typeList;
-    private List<String> dataList;
-    private List<String> successToast;
-    private List<String> failToast;
-    private List<String> postData;
-    private List<Integer> token;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public int getTime() {
-        return time;
-    }
-
-    public void setTime(int time) {
-        this.time = time;
-    }
-
-    public List<String> getImgUrlList() {
-        return imgUrlList;
-    }
-
-    public void setImgUrlList(List<String> imgUrlList) {
-        this.imgUrlList = imgUrlList;
-    }
-
-    public List<String> getTextList() {
-        return textList;
-    }
-
-    public void setTextList(List<String> textList) {
-        this.textList = textList;
-    }
-
-    public List<String> getTypeList() {
-        return typeList;
-    }
-
-    public void setTypeList(List<String> typeList) {
-        this.typeList = typeList;
-    }
-
-    public List<String> getDataList() {
-        return dataList;
-    }
-
-    public void setDataList(List<String> dataList) {
-        this.dataList = dataList;
-    }
-
-    public List<String> getSuccessToast() {
-        return successToast;
-    }
-
-    public void setSuccessToast(List<String> successToast) {
-        this.successToast = successToast;
-    }
-
-    public List<String> getFailToast() {
-        return failToast;
-    }
-
-    public void setFailToast(List<String> failToast) {
-        this.failToast = failToast;
-    }
-
-    public List<String> getPostData() {
-        return postData;
-    }
-
-    public void setPostData(List<String> postData) {
-        this.postData = postData;
-    }
-
-    public List<Integer> getToken() {
-        return token;
-    }
-
-    public void setToken(List<Integer> token) {
-        this.token = token;
-    }
+    var code = 0
+    var time = 0
+    var imgUrlList: List<String> = emptyList()
+    var textList: List<String> = emptyList()
+    var typeList: List<String> = emptyList()
+    var dataList: List<String> = emptyList()
+    var successToast: List<String> = emptyList()
+    var failToast: List<String> = emptyList()
+    var postData: List<String> = emptyList()
+    var token: List<Int> = emptyList()
 }
