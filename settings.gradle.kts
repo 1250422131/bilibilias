7d72dce54f27d9268afaecd2304f879ecf0647a0
sock_magic <startxingcheng@gmail.com>
2023-09-14 10:19:58 +0800

    fix(dev_compose): 修复仓库镜像源遗漏
    Former-commit-id: 39e2b4e9b33810af23d6bb674f57be636f19f4a4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 pluginManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/apache-snapshots")
         maven("https://jitpack.io")
         mavenCentral()
         gradlePluginPortal()
@@ -13,10 +14,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/apache-snapshots")
         maven("https://jitpack.io")
+
         mavenCentral()
         google()
     }
