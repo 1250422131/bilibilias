2fdfcd20d8ad587c79c4c3ccd08911d97a5a5644
sock_magic <startxingcheng@gmail.com>
2023-09-15 22:37:20 +0800

    refactor(dev_compose): 使用 timber 替换原生日志
    Former-commit-id: 1ebf49a55cf73ea28fe87e0b9b59cdbee5136549

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/RoutineUtils.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/RoutineUtils.kt
@@ -1,30 +1,29 @@
-package com.imcys.bilibilias.base.utils
+package com.imcys.bilibilias.common.base.utils
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
+import timber.log.Timber
 
+// 顶层方法
+fun asLogI(tag: String, content: String) = Timber.tag(tag).i(content)
 
-//顶层方法
-fun asLogI(tag: String, content: String) = Log.i(tag, content)
+fun asLogD(tag: String, content: String) = Timber.tag(tag).d(content)
 
-fun asLogD(tag: String, content: String) = Log.d(tag, content)
+fun asLogE(tag: String, content: String) = Timber.tag(tag).e(content)
 
-fun asLogE(tag: String, content: String) = Log.e(tag, content)
+fun asLogV(tag: String, content: String) = Timber.tag(tag).v(content)
 
-fun asLogV(tag: String, content: String) = Log.v(tag, content)
+fun asLoW(tag: String, content: String) = Timber.tag(tag).w(content)
 
-fun asLoW(tag: String, content: String) = Log.w(tag, content)
+fun asLogI(context: Context, content: String) = Timber.tag(context::class.java.simpleName).i(content)
 
-fun asLogI(context: Context, content: String) = Log.i(context::class.java.simpleName, content)
+fun asLogD(context: Context, content: String) = Timber.tag(context::class.java.simpleName).d(content)
 
-fun asLogD(context: Context, content: String) = Log.d(context::class.java.simpleName, content)
+fun asLogE(context: Context, content: String) = Timber.tag(context::class.java.simpleName).e(content)
 
-fun asLogE(context: Context, content: String) = Log.e(context::class.java.simpleName, content)
+fun asLogV(context: Context, content: String) = Timber.tag(context::class.java.simpleName).v(content)
 
-fun asLogV(context: Context, content: String) = Log.v(context::class.java.simpleName, content)
-
-fun asLoW(context: Context, content: String) = Log.w(context::class.java.simpleName, content)
+fun asLoW(context: Context, content: String) = Timber.tag(context::class.java.simpleName).w(content)
 
 fun asToast(context: Context, content: String) =
     Toast.makeText(context, content, Toast.LENGTH_SHORT).show()
