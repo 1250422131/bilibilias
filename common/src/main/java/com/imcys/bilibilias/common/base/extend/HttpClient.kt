726fbb876d7e2a9f5361392bcaee5e638840192f
sock_magic <startxingcheng@gmail.com>
2023-09-15 10:21:50 +0800

    feat(dev_compose): http 扩展函数

--- common/src/main/java/com/imcys/bilibilias/common/base/extend/HttpClient.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/extend/HttpClient.kt
@@ -6,15 +6,18 @@ import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.ResponseException
 import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
 import io.ktor.client.request.request
+import io.ktor.utils.io.errors.IOException
 import kotlinx.serialization.SerializationException
-import java.io.IOException
+import timber.log.Timber
+import kotlin.Result
 
 suspend inline fun <reified T, reified E> HttpClient.safeRequest(
     block: HttpRequestBuilder.() -> Unit,
 ): ApiResponse<T, E> =
     try {
-        val response = request { block() }
+        val response = request(block)
         ApiResponse.Success(response.body())
     } catch (e: ClientRequestException) {
         ApiResponse.Error.HttpError(e.response.status.value, e.errorBody())
@@ -24,6 +27,27 @@ suspend inline fun <reified T, reified E> HttpClient.safeRequest(
         ApiResponse.Error.NetworkError
     } catch (e: SerializationException) {
         ApiResponse.Error.SerializationError
+    }
+
+suspend inline fun <reified T> HttpClient.safeGet(
+    url: String,
+    block: HttpRequestBuilder.() -> Unit,
+): Result<T> =
+    try {
+        val response = get(url, block)
+        Result.success(response.body())
+    } catch (e: ClientRequestException) {
+        Timber.tag("SafeGetCatching").d(e,"客户端异常")
+        Result.failure(e)
+    } catch (e: ServerResponseException) {
+        Timber.tag("SafeGetCatching").d(e,"服务器异常")
+        Result.failure(e)
+    } catch (e: IOException) {
+        Timber.tag("SafeGetCatching").d(e)
+        Result.failure(e)
+    } catch (e: SerializationException) {
+        Timber.tag("SafeGetCatching").d(e,"序列化失败")
+        Result.failure(e)
     }
 
 suspend inline fun <reified E> ResponseException.errorBody(): E? =
