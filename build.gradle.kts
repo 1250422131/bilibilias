610c6b71da2b928a3e4ed42d496dda7721de70df
sock_magic <startxingcheng@gmail.com>
2023-09-20 00:34:08 +0800

    build(dev_compose): 更新
    Former-commit-id: 0373ef189a28d9d41d2080630fa2583347320dda

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.detekt)
+    kotlin("kapt") version "1.9.10"
 }
 
 detekt {
@@ -17,17 +18,30 @@ detekt {
     basePath = rootDir.absolutePath
     autoCorrect = true
     allRules = true
-    disableDefaultRuleSets = true
+    dependencies {
+        detektPlugins(libs.gitlab.detekt.formatting)
+        detektPlugins(libs.detekt.rules.compose)
+        detektPlugins(libs.detekt)
+        detektPlugins(libs.hbmartin.detekt.rules)
+        detektPlugins(libs.rules.detekt)
+    }
 }
 
 val reportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
-    output.set(rootProject.layout.buildDirectory.file("$rootDir/config/reports/merge.xml"))
     output.set(rootProject.layout.buildDirectory.file("$rootDir/config/reports/merge.sarif"))
 }
 
 subprojects {
     apply(plugin = "io.gitlab.arturbosch.detekt")
-
+    detekt {
+        toolVersion = "1.23.1"
+        config.setFrom(file("$rootDir/config/detekt.yml"))
+        baseline = file("$rootDir/config/reports/baseline.xml")
+        parallel = true
+        basePath = rootDir.absolutePath
+        autoCorrect = true
+        allRules = true
+    }
     tasks.named("detekt", io.gitlab.arturbosch.detekt.Detekt::class).configure {
         reports {
             xml.required.set(true)
@@ -43,16 +57,8 @@ subprojects {
     }
 
     reportMerge {
-        input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map { it.xmlReportFile }) // or .sarifReportFile
+        input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map { it.sarifReportFile })
     }
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    this.jvmTarget = "17"
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
-    this.jvmTarget = "17"
 }
 
 configurations.matching { it.name == "detekt" }.all {
@@ -62,7 +68,12 @@ configurations.matching { it.name == "detekt" }.all {
         }
     }
 }
-
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    this.jvmTarget = "17"
+}
+tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+    this.jvmTarget = "17"
+}
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
