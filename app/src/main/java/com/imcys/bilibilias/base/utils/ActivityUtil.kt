ca3222f643d9255bbf906ba1744f29ebfe6e230b
sock_magic <startxingcheng@gmail.com>
2023-09-21 09:29:13 +0800

    refactor(dev_compose): 添加默认值

--- app/src/main/java/com/imcys/bilibilias/base/utils/ActivityUtil.kt
+++ app/src/main/java/com/imcys/bilibilias/base/utils/ActivityUtil.kt
@@ -1,11 +1,15 @@
 package com.imcys.bilibilias.base.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 
-fun Context.startActivity(clazz: Class<*>) {
-    val intent = Intent(this, clazz)
+fun Context.startActivity(clazz: Class<out Activity>, bundle: Bundle = Bundle()) {
+    val intent = Intent(this, clazz).apply {
+        putExtras(bundle)
+    }
     startActivity(intent)
 }
 
