a3b021de5c61f918ca8a9e2f07ebdbd1c6ff9fcd
sock_magic <startxingcheng@gmail.com>
2023-09-21 09:29:42 +0800

    refactor(dev_compose): 统一在类上使用注解

--- common/src/main/java/com/imcys/bilibilias/common/di/AppModule.kt
+++ common/src/main/java/com/imcys/bilibilias/common/di/AppModule.kt
@@ -1,17 +1,9 @@
 package com.imcys.bilibilias.common.di
 
-import com.imcys.bilibilias.common.base.repository.VideoRepository
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.ktor.client.HttpClient
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
-    @Provides
-    @Singleton
-    fun provideVideoRepository(httpClient: HttpClient) = VideoRepository(httpClient)
-}
+class AppModule
