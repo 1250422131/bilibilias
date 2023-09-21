47829cf27c593b476d564ae8972eb5170e7d3e4c
sock_magic <startxingcheng@gmail.com>
2023-09-21 09:36:05 +0800

    refactor(dev_compose): 由仓库层发起网络请求

--- common/src/main/java/com/imcys/bilibilias/common/base/repository/UserRepository.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/repository/UserRepository.kt
@@ -1,24 +1,34 @@
 package com.imcys.bilibilias.common.base.repository
 
 import com.imcys.bilibilias.common.base.api.BilibiliApi
+import com.imcys.bilibilias.common.base.extend.safeGet
+import com.imcys.bilibilias.common.base.model.UserCardBean
 import com.imcys.bilibilias.common.base.model.UserSpaceInformation
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class UserRepository @Inject constructor(
     private val httpClient: HttpClient,
     private val wbiRepository: WbiKeyRepository
 ) {
+    /**
+     * todo 也许可以返回文本，来进行解析
+     */
+    suspend fun get用户名片信息(mid: Long) =
+        httpClient.safeGet<UserCardBean>(BilibiliApi.getUserCardPath) {
+            parameter("mid", mid)
+        }.getOrThrow()
+
     suspend fun getUserSpaceDetails(mid: Long): UserSpaceInformation {
         val params = wbiRepository.getUserNavToken(listOf("mid" to mid.toString()))
-        val information = httpClient.get(BilibiliApi.userSpaceDetails) {
+        val information = httpClient.safeGet<UserSpaceInformation>(BilibiliApi.userSpaceDetails) {
             params.forEach { (k, v) ->
                 parameter(k, v)
             }
-        }.body<UserSpaceInformation>()
+        }.getOrThrow()
         return information
     }
 }
