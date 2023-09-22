86fd2c0a56f7cd4302815e463597fb834f046ec8
sock_magic <startxingcheng@gmail.com>
2023-09-22 09:41:08 +0800

    refactor(dev_compose): 统一文件名，添加从context得到activity
    Former-commit-id: 870bcf669c942b8af7b8156a17983076c505a505

--- app/src/main/java/com/imcys/bilibilias/base/utils/ActivityUtil.kt
+++ app/src/main/java/com/imcys/bilibilias/base/utils/ActivityUtil.kt
@@ -2,9 +2,11 @@ package com.imcys.bilibilias.base.utils
 
 import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 fun Context.startActivity(clazz: Class<out Activity>, bundle: Bundle = Bundle()) {
     val intent = Intent(this, clazz).apply {
@@ -17,4 +19,9 @@ fun Context.openUri(uri: Uri) {
     val intent = Intent(Intent.ACTION_VIEW, uri)
     // 如果不支持的 Uri 将抛出异常
     startActivity(intent)
+}
+fun Context.getActivity(): AppCompatActivity? = when (this) {
+    is AppCompatActivity -> this
+    is ContextWrapper -> baseContext.getActivity()
+    else -> null
 }
