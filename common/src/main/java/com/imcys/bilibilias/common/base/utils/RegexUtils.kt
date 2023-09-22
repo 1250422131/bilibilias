3c0337e18cd9703d4f629dc46bef384b66d0cc4f
sock_magic <startxingcheng@gmail.com>
2023-09-22 09:34:45 +0800

    refactor(dev_compose): 正则表达式更新
    Former-commit-id: 5c01c71f04e48df6d9777b032814c34bccb1df6a

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/RegexUtils.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/RegexUtils.kt
@@ -6,12 +6,12 @@ object RegexUtils {
     // bv过滤
     val bvRegex by lazy(
         LazyThreadSafetyMode.NONE
-    ) { Regex("""(BV|bv|Bv|bV)1([A-z]|[0-9]){2}4([A-z]|[0-9])1([A-z]|[0-9])7([A-z]|[0-9]){2}""") }
+    ) { Regex("""(BV|bv|Bv|bV)([a-z|A-Z|0-9]{10})""", RegexOption.IGNORE_CASE) }
 
     val bvHttpRegex by lazy(LazyThreadSafetyMode.NONE) {
         Regex("""https://b23.tv/([A-z]|\d)*""")
     }
 
     // av过滤
-    val avRegex by lazy(LazyThreadSafetyMode.NONE) { Regex("""(?<=(av|aV|AV|Av))([0-9]+)""") }
+    val avRegex by lazy(LazyThreadSafetyMode.NONE) { Regex("""(?<=(av))([0-9]+)""", RegexOption.IGNORE_CASE) }
 }
