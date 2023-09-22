332fd300312196c4356fe61e480b524aaeca8b09
sock_magic <startxingcheng@gmail.com>
2023-09-22 09:43:31 +0800

    refactor(dev_compose): 统一文件名， 可以解析ep，av，bv，短链接
    Former-commit-id: b4216a7a12f07b9d51e2a19fdf0c2e4ed6408d19

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/AsVideoUtils.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/AsVideoUtils.kt
@@ -1,25 +1,35 @@
 package com.imcys.bilibilias.common.base.utils
 
-object AsVideoNumUtils {
+object AsVideoUtils {
 
     /**
      * 是否是番剧
      */
     fun isEp(text: String) = RegexUtils.epRegex.containsMatchIn(text)
-    fun isBV(text: String) = RegexUtils.bvRegex.containsMatchIn(text)
+    fun isBVStart(text: String) = RegexUtils.bvRegex.containsMatchIn(text)
+    fun isBVHttp(text: String) = RegexUtils.bvHttpRegex.containsMatchIn(text)
     fun isAV(text: String) = RegexUtils.avRegex.containsMatchIn(text)
     fun getEpid(text: String): String? {
         if (isEp(text)) {
-            return RegexUtils.bvRegex.find(text)?.value
+            return RegexUtils.epRegex.find(text)?.value
         }
         return null
     }
+
+    fun getBvHttp(text: String): String? {
+        if (isBVHttp(text)) {
+            return RegexUtils.bvHttpRegex.find(text)?.value
+        }
+        return null
+    }
+
     fun getBvid(text: String): String? {
-        if (isBV(text)) {
+        if (isBVStart(text)) {
             return RegexUtils.bvRegex.find(text)?.value
         }
         return null
     }
+
     fun getAvid(text: String): String? {
         if (isAV(text)) {
             return RegexUtils.avRegex.find(text)?.value
