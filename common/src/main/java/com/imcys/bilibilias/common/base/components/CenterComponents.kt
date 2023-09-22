fd6ac3252262c1ac2133a2264cdb580f0125d36b
sock_magic <startxingcheng@gmail.com>
2023-09-22 09:39:00 +0800

    feat(dev_compose): 列居中函数

--- common/src/main/java/com/imcys/bilibilias/common/base/components/CenterComponents.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/components/CenterComponents.kt
@@ -1,6 +1,8 @@
 package com.imcys.bilibilias.common.base.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,4 +13,17 @@ import androidx.compose.ui.Modifier
 @Composable
 inline fun CenterRow(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
     Row(modifier.fillMaxWidth(), Arrangement.Center, Alignment.CenterVertically, content)
+}
+
+@Composable
+inline fun CenterColumn(modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit) {
+    Column(modifier, Arrangement.Center, Alignment.CenterHorizontally, content)
+}
+
+@Composable
+fun VerticalTwoTerms(top: @Composable () -> Unit, bottom: @Composable () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier, Arrangement.Center, Alignment.CenterHorizontally) {
+        top()
+        bottom()
+    }
 }
