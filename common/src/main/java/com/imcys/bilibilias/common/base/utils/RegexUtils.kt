c54d5536a8cf7752571ce9beaba23defc04e9274
sock_magic <startxingcheng@gmail.com>
2023-09-26 02:44:20 +0800

    refactor(dev_compose): 新正则表达式

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/RegexUtils.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/RegexUtils.kt
@@ -6,7 +6,10 @@ object RegexUtils {
     // bv过滤
     val bvRegex by lazy(
         LazyThreadSafetyMode.NONE
-    ) { Regex("""^bv[\da-zA-Z]{10}${'$'}""", RegexOption.IGNORE_CASE) }
+    ) { Regex("""[BVvb][A-Za-z0-9]{11}""", RegexOption.IGNORE_CASE) }
+    val bvRegex1 by lazy(
+        LazyThreadSafetyMode.NONE
+    ) { Regex("""(?<=BV|Bv|bv|bV)[A-Za-z0-9]{10}""", RegexOption.IGNORE_CASE) }
 
     val shortLink by lazy(LazyThreadSafetyMode.NONE) {
         Regex("""https://b23.tv/[a-zA-Z0-9]+${'$'}""")
