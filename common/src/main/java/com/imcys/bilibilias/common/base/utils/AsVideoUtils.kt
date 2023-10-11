88f201a0348e40cf5c296acf1676b9a9926b2181
sock_magic <startxingcheng@gmail.com>
2023-10-11 17:48:54 +0800

    refactor(dev_compose): bv正则修改
    Former-commit-id: 0b04334a8de20c1e5cb7b72b10cc05d3314d7e54

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/AsVideoUtils.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/AsVideoUtils.kt
@@ -28,7 +28,7 @@ object AsVideoUtils {
 
     fun getBvid(text: String): String? {
         if (isBV(text)) {
-            return RegexUtils.bvRegex.find(text)?.value
+            return "BV" + RegexUtils.bvRegex.find(text)?.value
         }
         return null
     }
@@ -46,10 +46,7 @@ object AsVideoUtils {
             30232 -> "132K"
             30250 -> "杜比全景声"
             30251 -> "Hi-Res无损"
-            30280 -> "192K"
-            else -> {
-                "192K"
-            }
+            else -> "192K"
         }
     }
 }
