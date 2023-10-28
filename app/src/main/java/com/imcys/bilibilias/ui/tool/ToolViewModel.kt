ab0f6d5d03d36404f488407b4af3ce34765d9814
sock_magic <startxingcheng@gmail.com>
2023-10-28 10:39:55 +0800

    refactor(dev_compose): 重构工具解析

--- app/src/main/java/com/imcys/bilibilias/ui/tool/ToolViewModel.kt
+++ app/src/main/java/com/imcys/bilibilias/ui/tool/ToolViewModel.kt
@@ -1,7 +1,12 @@
 package com.imcys.bilibilias.ui.tool
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.viewModelScope
 import com.imcys.bilibilias.common.base.extend.Result
+import com.imcys.bilibilias.common.base.extend.digitalConversion
 import com.imcys.bilibilias.common.base.model.bangumi.Bangumi
 import com.imcys.bilibilias.common.base.model.video.VideoDetails
 import com.imcys.bilibilias.common.base.repository.VideoRepository
@@ -9,9 +14,15 @@ import com.imcys.bilibilias.common.base.utils.AsVideoUtils
 import com.imcys.bilibilias.home.ui.viewmodel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.ktor.client.statement.request
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.serialization.json.Json
@@ -26,6 +37,8 @@ class ToolViewModel @Inject constructor(
 
     private val _toolState = MutableStateFlow(ToolState())
     val toolState = _toolState.asStateFlow()
+    var inputText by mutableStateOf("")
+        private set
 
     init {
         videoRepository.videoDetails2
@@ -33,7 +46,17 @@ class ToolViewModel @Inject constructor(
                 when (res) {
                     is Result.Error -> TODO()
                     Result.Loading -> {}
-                    is Result.Success -> _toolState.update { it.copy(videoDetails = res.data, isShowVideoCard = true) }
+                    is Result.Success -> _toolState.update {
+                        val data = res.data
+                        it.copy(
+                            isShowVideoCard = true,
+                            pic = data.pic,
+                            title = data.title,
+                            desc = data.descV2?.first()?.rawText ?: data.desc,
+                            view = data.stat.view.digitalConversion(),
+                            danmaku = data.stat.danmaku.digitalConversion(),
+                        )
+                    }
                 }
             }.launchIn(viewModelScope)
 
@@ -51,7 +74,30 @@ class ToolViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    private fun parseTextTypes(text: String): VideoType {
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    val result = snapshotFlow { inputText }
+        .filterNot { it.isBlank() }
+        .debounce(300)
+        .distinctUntilChanged()
+        .mapLatest { parsingTextTypes(it) }
+        .onEach { type ->
+            Timber.tag(TAG).i("解析结果=$type")
+            when (type) {
+                is VideoType.AV -> handelAV(type.id)
+                is VideoType.BV -> handelBV(type.id)
+                is VideoType.EP -> handelEP(type.id)
+                is VideoType.ShortLink -> handelHttp(type.url)
+                VideoType.None -> _toolState.update {
+                    it.copy(isShowVideoCard = false, inputError = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+
+    fun updateInput(newText: String) {
+        inputText = newText
+    }
+
+    private fun parsingTextTypes(text: String): VideoType {
         return if (AsVideoUtils.isAV(text)) {
             val id = AsVideoUtils.getAvid(text)
             if (id != null) VideoType.AV(id) else VideoType.None
@@ -70,19 +116,6 @@ class ToolViewModel @Inject constructor(
     }
 
     fun parsesBvOrAvOrEp(text: String) {
-        if (text.trim().isBlank()) return
-        val type = parseTextTypes(text)
-        Timber.tag(TAG).i("解析结果=$type")
-        _toolState.update { it.copy(text = text, videoType = type) }
-        when (type) {
-            is VideoType.AV -> handelAV(type.id)
-            is VideoType.BV -> handelBV(type.id)
-            is VideoType.EP -> handelEP(type.id)
-            is VideoType.ShortLink -> handelHttp(type.url)
-            VideoType.None -> _toolState.update {
-                it.copy(isShowVideoCard = false, inputError = true)
-            }
-        }
     }
 
     private fun handelEP(id: String) {
@@ -96,26 +129,21 @@ class ToolViewModel @Inject constructor(
             when (val shortLink = videoRepository.shortLink(url)) {
                 is Result.Error -> TODO()
                 Result.Loading -> {}
-                is Result.Success -> parseTextTypes(shortLink.data.request.url.toString())
+                is Result.Success -> parsingTextTypes(shortLink.data.request.url.toString())
             }
         }
     }
 
-    private fun handelAV(id: String) {
-        launchIO {
-            videoRepository.getVideoDetailsByAid(id)
-        }
-    }
+    private suspend fun handelAV(id: String): Result<VideoDetails> =
+        videoRepository.getVideoDetailsByAid(id)
 
-    private fun handelBV(id: String) {
-        launchOnIO {
-            videoRepository.getVideoDetailsByBvid(id)
-        }
-    }
+    private suspend fun handelBV(id: String): Result<VideoDetails> =
+        videoRepository.getVideoDetailsByBvid(id)
 
-    fun clearText() {
+    fun clearInput() {
+        inputText = ""
         _toolState.update {
-            it.copy(text = "", inputError = false, isShowVideoCard = false)
+            it.copy(inputError = false, isShowVideoCard = false)
         }
     }
 }
@@ -129,12 +157,15 @@ sealed class VideoType {
 }
 
 data class ToolState(
-    val text: String = "",
     val inputError: Boolean = false,
     val isShowVideoCard: Boolean = false,
-    val videoDetails: VideoDetails = VideoDetails(),
     val bangumi: Bangumi.Result = Bangumi.Result(),
-    val videoType: VideoType = VideoType.None
+    val videoType: VideoType = VideoType.None,
+    val pic: String = "",
+    val title: String = "",
+    val desc: String = "",
+    val view: String = "",
+    val danmaku: String = "",
 )
 
 private const val TAG = "ToolViewModel"
