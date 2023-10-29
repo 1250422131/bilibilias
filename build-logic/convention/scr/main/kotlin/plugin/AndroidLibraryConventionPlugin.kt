319dac908fd5b366c34d1a2d73b0fc97b2ae134a
sock_magic <startxingcheng@gmail.com>
2023-10-29 13:37:27 +0800

    build(dev_compose): 添加构建逻辑模块
    Former-commit-id: 124e2932f5b28fe1712ac7696f87d9a202c0238d

---
+++ build-logic/convention/scr/main/kotlin/plugin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,48 @@
+package plugin
+
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.PluginManager
+import org.gradle.kotlin.dsl.dependencies
+
+@Suppress("unused")
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        with(project) {
+            pluginManager.applyPlugin()
+            applyAndroid<LibraryExtension> {
+                configureKotlin(this)
+                configureAndroid()
+            }
+            applyDependencies()
+        }
+    }
+
+    private fun PluginManager.applyPlugin() {
+        apply("com.android.library")
+        apply("org.jetbrains.kotlin.android")
+        apply("kotlin-parcelize") // TODO
+    }
+
+    private fun LibraryExtension.configureAndroid() {
+        configureAndroidCommon()
+
+        buildTypes {
+            release {
+                isMinifyEnabled = true
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+    }
+
+    private fun Project.applyDependencies() {
+        dependencies {
+            implementationDefaultTestDependencies(libs)
+        }
+    }
+}
