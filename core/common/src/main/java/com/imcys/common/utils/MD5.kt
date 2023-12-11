54930f4d0596f2e9e395fcd5fde01585653fdb86
sock_magic <startxingcheng@gmail.com>
2023-12-11 09:54:51 +0800

    chore: update
    Former-commit-id: 6c3d620e44171c86ff6f6db5d3a6245598e2206d

--- core/common/src/main/java/com/imcys/common/utils/MD5.kt
+++ core/common/src/main/java/com/imcys/common/utils/MD5.kt
@@ -1,9 +1,12 @@
 package com.imcys.common.utils
 
+import android.content.pm.Signature
+import dev.utils.app.SignaturesUtils
 import java.math.BigInteger
 import java.security.MessageDigest
 
 fun md5(input: String): String {
     val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    return SignaturesUtils.signatureMD5(arrayOf(Signature(input)))
 }
