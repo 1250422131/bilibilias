d5da405779d68b835d62ddc8b8603651fc6a1a01
sock_magic <startxingcheng@gmail.com>
2023-12-14 10:50:44 +0800

    build: 添加新库
    Former-commit-id: 5bee2cf70f06bc2c88a6177b11e713ad55b6c084

--- core/crash/build.gradle.kts
+++ core/crash/build.gradle.kts
@@ -12,4 +12,12 @@ android {
 
 dependencies {
     implementation(libs.androidx.annotation.jvm)
+    implementation(libs.acra.http)
+    implementation(libs.acra.mail)
+    implementation(libs.acra.core)
+    implementation(libs.acra.dialog)
+    implementation(libs.acra.notification)
+    implementation(libs.acra.toast)
+    implementation(libs.acra.limiter)
+    implementation(libs.acra.scheduler)
 }
