a1163379fe8e58368dbbb9307b7f368a9e2a9d6a
sock_magic <startxingcheng@gmail.com>
2023-12-25 11:04:37 +0800

    refactor: 更新
    Former-commit-id: 62e38c4b68178bbb1e821c9599c74ea0a82cac78

--- feature/download/src/main/java/com/bilias/feature/download/DownloadViewModel.kt
+++ feature/download/src/main/java/com/bilias/feature/download/DownloadViewModel.kt
@@ -2,17 +2,27 @@ package com.bilias.feature.download
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.imcys.model.download.Entry
 import com.imcys.network.download.IDownloadManage
 import com.imcys.network.download.downloadDir
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class DownloadViewModel @Inject constructor(
     private val downloadManager: IDownloadManage
 ) : ViewModel() {
+    private val _taskState = MutableStateFlow(TaskState())
+    val taskState = _taskState.asStateFlow()
 
     fun taskList(context: Context) {
         val allTask = downloadManager.getAllTask(context.downloadDir.absolutePath)
+            .map(Entry::mapToTask)
+            .toImmutableList()
+        _taskState.update { it.copy(taskList = allTask) }
     }
 }
