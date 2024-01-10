feb6840c158a898960694296bf7881650137ed12
sock_magic <startxingcheng@gmail.com>
2024-01-10 11:29:40 +0800

    chore: 添加日志记录错误

--- core/common/src/main/java/com/imcys/common/utils/Result.kt
+++ core/common/src/main/java/com/imcys/common/utils/Result.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
@@ -14,5 +15,5 @@ sealed interface Result<out T> {
 fun <T> Flow<T>.asResult(): Flow<Result<T>> {
     return map<T, Result<T>> { Result.Success(it) }
         .onStart { emit(Result.Loading) }
-        .catch { emit(Result.Error(it)) }
+        .catch { Timber.e(it); emit(Result.Error(it)) }
 }
