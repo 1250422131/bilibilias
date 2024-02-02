6606bc7287c250f26ccfb1d6f65e9c5cd7d4ff97
sock_magic <startxingcheng@gmail.com>
2024-02-02 13:32:19 +0800

    refactor: 发送所有 cookie

--- core/network/src/main/java/com/imcys/network/configration/CookieManager.kt
+++ core/network/src/main/java/com/imcys/network/configration/CookieManager.kt
@@ -1,15 +1,12 @@
 package com.imcys.network.configration
 
-import androidx.collection.ArrayMap
 import com.imcys.datastore.fastkv.ICookieStore
 import io.ktor.client.plugins.cookies.CookiesStorage
 import io.ktor.http.Cookie
 import io.ktor.http.Url
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.cbor.Cbor
-import kotlinx.serialization.protobuf.ProtoBuf
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,17 +16,16 @@ import javax.inject.Singleton
 class CookieManager
 @Inject constructor(
     private val cookiesData: ICookieStore,
-    private val cbor: Cbor,
-    private val protoBuf: ProtoBuf
+    private val cbor: Cbor
 ) : CookiesStorage {
-    private val cache = ArrayMap<String, Cookie>(5)
-    private val sterileMap = MapSerializer(String.serializer(), CookieSerializer)
+    private val cache = mutableListOf<Cookie>()
+    private val sterileMap = ListSerializer(CookieSerializer)
 
     init {
         if (cookiesData.valid) {
             cookiesData.get()?.let { bytes ->
-                cbor.decodeFromByteArray(sterileMap, bytes).map { (k, v) ->
-                    cache.put(k, v)
+                cbor.decodeFromByteArray(sterileMap, bytes).map {
+                    cache.add(it)
                 }
             }
         }
@@ -37,23 +33,15 @@ class CookieManager
 
     override suspend fun addCookie(requestUrl: Url, cookie: Cookie) {
         Timber.d("addCookie: $cookie")
-        val timestamp = cookie.expires?.timestamp ?: return
-        if (timestamp < System.currentTimeMillis()) {
-            return
-        }
-        cache[cookie.name] = cookie
+        val timestamp = cookie.expires?.timestamp ?: 0
+        cache += cookie
         cookiesData.setTime(timestamp)
         save()
     }
 
     override suspend fun get(requestUrl: Url): List<Cookie> {
         Timber.d("getCookie: $requestUrl")
-        val cookie = cache[SESSION_DATA]
-        return if (cookie == null) {
-            listOf()
-        } else {
-            listOf(cookie)
-        }
+        return cache
     }
 
     override fun close() {
@@ -69,5 +57,3 @@ class CookieManager
         )
     }
 }
-
-const val SESSION_DATA = "SESSDATA"
