e690aa124be53e56a6a87e6e5d84bbd4cf34e0f0
sock_magic <startxingcheng@gmail.com>
2024-02-03 22:37:19 +0800

    refactor: 删除不必要路径

--- core/network/src/main/java/com/imcys/network/api/BilibiliApi2.kt
+++ core/network/src/main/java/com/imcys/network/api/BilibiliApi2.kt
@@ -4,18 +4,6 @@ object BilibiliApi2 {
     // <editor-fold desc="登录">
     const val loginApi = "https://passport.bilibili.com/"
 
-    // 检查 cookie 是否需要刷新
-    const val CHECK_COOKIE_REFRESH = loginApi + "x/passport-login/web/cookie/info"
-
-    // 刷新 cookie
-    const val COOKIE_REFRESH = loginApi + "x/passport-login/web/cookie/refresh"
-
-    // 让旧 cookie 失效
-    const val CONFIRM_REFRESH = loginApi + "x/passport-login/web/confirm/refresh"
-
-    // 刷新 cookie
-    const val CORRESPOND = "https://www.bilibili.com/correspond/1/"
-
     // 申请二维码
     const val getLoginQRPath = loginApi + "x/passport-login/web/qrcode/generate"
 
@@ -26,40 +14,14 @@ object BilibiliApi2 {
     const val exitLogin = loginApi + "login/exit/v2"
     // </editor-fold>
 
-    // 获取个人基本信息
-    const val getMyUserData = "x/member/web/account"
-
-    // 获取用户卡片信息
-    const val getUserCardPath = "x/web-interface/card"
-
     // 番剧视频流
     const val PGC_PLAY_URL = "pgc/player/web/v2/playurl"
 
     // 获取剧集明细 season_id / ep_id
     const val PGC_VIEW_SEASON = "pgc/view/web/season"
 
-    // 修改视频收藏
-    const val videoCollectionSetPath = "x/v3/fav/resource/deal"
-
-    // 投币地址
-    const val videoCoinAddPath = "x/web-interface/coin/add"
-
-    // 获取用户基本信息
-    const val userSpaceDetails = "x/space/wbi/acc/info"
-
-    // 用户状态
-    const val userUpStat = "x/space/upstat"
-
-    // 用户投稿
-    const val userWorksPath = "x/space/wbi/arc/search"
-
-    // 播放历史
-    const val userPlayHistoryPath = "x/web-interface/history/cursor"
-
     // 追番剧列表
     const val bangumiFollowPath = "x/space/bangumi/follow/list"
-
-    const val Token = "x/web-interface/nav"
 
     /**
      * 收藏夹
