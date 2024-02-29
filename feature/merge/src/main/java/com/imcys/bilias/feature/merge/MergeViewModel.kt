7ba13feda66b2ec6a66e712387ae92a56463e260
sock_magic <startxingcheng@gmail.com>
2024-02-29 22:20:16 +0800

    refactor: 更新
    Former-commit-id: 559ccbea04b9671aee7c70a695a2158aac245797

--- feature/merge/src/main/java/com/imcys/bilias/feature/merge/MergeViewModel.kt
+++ feature/merge/src/main/java/com/imcys/bilias/feature/merge/MergeViewModel.kt
@@ -4,8 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.imcys.bilias.feature.merge.danmaku.IDanmakuParse
-import com.imcys.bilias.feature.merge.mix.FFmpegMerge
-import com.imcys.bilias.feature.merge.mix.MergeData
+import com.imcys.bilias.feature.merge.mix.MixVideoAudio
 import com.imcys.common.di.AsDispatchers
 import com.imcys.common.di.Dispatcher
 import com.imcys.model.download.Entry
@@ -14,14 +13,13 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class MergeViewModel @Inject constructor(
-    private val ffmpegMerge: FFmpegMerge,
+    private val ffmpegMerge: MixVideoAudio,
     private val xmlToAss: IDanmakuParse,
     @Dispatcher(AsDispatchers.IO) private val ioDispatchers: CoroutineDispatcher,
     private val downloadManage: DownloadManage
@@ -34,43 +32,10 @@ class MergeViewModel @Inject constructor(
 
     private val resource = mutableListOf<Entry>()
 
-    private val listener = object : FFmpegMerge.Listener {
-        override fun onProgress(progress: Int, pts: Long) {
-            _uiState.update {
-                it.copy(progress = progress)
-            }
-        }
-
-        override fun onError(errorCode: Int, errorMsg: String?, mixFile: String, realName: String) {
-            _uiState.update {
-                it.copy(errorMessage = "$realName 合并错误")
-            }
-        }
-
-        override fun onComplete() {
-            _uiState.update {
-                it.copy(complete = true, startMix = false)
-            }
-        }
-
-        override fun onStart(title: String) {
-            _uiState.update {
-                it.copy(current = "当前任务 $title", complete = false, startMix = true)
-            }
-        }
-
-        override fun onSuccess(fullPath: String, title: String, tasks: Int) {
-            _uiState.update {
-                it.copy(mixMessage = "合并成功 $title, 剩余 $tasks 任务")
-            }
-        }
-    }
-
     init {
         viewModelScope.launch(ioDispatchers) {
             startScanner()
         }
-        ffmpegMerge.setListener(listener)
     }
 
     fun selectResource(e: Entry, selected: Boolean) {
@@ -89,10 +54,6 @@ class MergeViewModel @Inject constructor(
 //            sendMixData(vFile, aFile, tempFile.absolutePath, entry.title)
 //            updatePhotoMedias(context, tempFile)
 //        }
-    }
-
-    private fun sendMixData(vFile: File, aFile: File, file: String, title: String) {
-        ffmpegMerge.execute(MergeData(vFile, aFile, file, title))
     }
 
     private fun startScanner() {
