49d20f4ceb7779ec02274388ddd2839132b084e7
sock_magic <startxingcheng@gmail.com>
2024-03-06 11:05:28 +0800

    chore: 修复约定插件
    Former-commit-id: 42b38ce9046d8393385010c6edf3fa0b797cefcd

--- build-logic/convention/src/main/kotlin/plugin/extension/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/plugin/extension/KotlinAndroid.kt
@@ -13,7 +13,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  * Configure base Kotlin with Android options
  */
 internal fun Project.configureKotlinAndroid(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
     commonExtension.apply {
         compileSdk = 34
