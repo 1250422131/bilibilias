d208c0c5d7f04cc12001e8c2b5828908ef915948
sock_magic <startxingcheng@gmail.com>
2024-03-25 17:50:38 +0800

    refactor: 添加子模块
    Former-commit-id: af9e5fc7d2d174a400ebdf1a3432c19f54ce980d

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,15 @@
+﻿plugins {
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.library.jacoco)
+    alias(libs.plugins.bilibilias.android.hilt)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.core.datastore.proto"
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ core/model/build.gradle.kts
@@ -0,0 +1,15 @@
+﻿plugins {
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.library.jacoco)
+    alias(libs.plugins.bilibilias.android.hilt)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.core.model"
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
