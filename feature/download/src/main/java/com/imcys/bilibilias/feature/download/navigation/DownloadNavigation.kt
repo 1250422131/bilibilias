a43ef16afe5de0b9eedfcc630dff2384ed0956c2
sock_magic <startxingcheng@gmail.com>
2024-04-02 11:21:31 +0800

    refactor: 添加子模块

---
+++ feature/download/src/main/java/com/imcys/bilibilias/feature/download/navigation/DownloadNavigation.kt
@@ -0,0 +1,11 @@
+﻿package com.imcys.bilibilias.feature.download.navigation
+
+import androidx.compose.runtime.Composable
+import com.imcys.bilibilias.feature.download.DownloadRoute
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun NavigationToDownload() {
+    DownloadRoute()
+}
---
+++ feature/user/src/main/java/com/imcys/bilibilias/feature/user/UserViewModel.kt
@@ -0,0 +1,9 @@
+﻿package com.imcys.bilibilias.feature.user
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor() : ViewModel() {
+}
