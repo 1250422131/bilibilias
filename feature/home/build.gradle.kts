1bcd0ff3d7f86bcfbb06b9e5744a9c089226b9fe
sock_magic <startxingcheng@gmail.com>
2024-04-02 10:22:32 +0800

    refactor: 添加主页模块

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,15 @@
+﻿plugins {
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.library.compose)
+    alias(libs.plugins.bilibilias.android.hilt)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.feature.home"
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
