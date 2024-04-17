0a57245cc608a8e1c34ab56ad6a3eee3ed87bba6
sock_magic <startxingcheng@gmail.com>
2024-04-17 20:45:13 +0800

    refactor: 添加未实现提示
    Former-commit-id: d3bfc1ddba4bd9b694bcd9f85873e0c05b525fb5

--- feature/user/src/main/java/com/imcys/bilibilias/feature/user/UserScreen.kt
+++ feature/user/src/main/java/com/imcys/bilibilias/feature/user/UserScreen.kt
@@ -1,10 +1,23 @@
 package com.imcys.bilibilias.feature.user
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun UserRoute(modifier: Modifier) {
+    Scaffold { innerPading ->
+        Column(
+            modifier = Modifier.padding(innerPading),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "当前页面未实现")
+        }
+    }
 }
 
 @Composable
