ccf5f241d4d2308885ad6fb98ca6f0df29383360
sock_magic <startxingcheng@gmail.com>
2024-04-30 22:24:32 +0800

    feat: 添加数据模块

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.library.jacoco)
+    alias(libs.plugins.bilibilias.android.hilt)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.core.data"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+dependencies {
+    api(projects.core.common)
+    api(projects.core.database)
+    api(projects.core.datastore)
+    api(projects.core.network)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
