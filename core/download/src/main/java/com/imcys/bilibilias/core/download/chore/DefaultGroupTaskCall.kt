e30be1d51be5b95c420fd8cb780aa4ab9e361de0
sock_magic <startxingcheng@gmail.com>
2024-05-09 16:08:11 +0800

    refactor: 完善合并功能
    Former-commit-id: 83f5ee7ddca55d49cc78699e1c2bca68f1adcf9c

--- core/download/src/main/java/com/imcys/bilibilias/core/download/chore/DefaultGroupTaskCall.kt
+++ core/download/src/main/java/com/imcys/bilibilias/core/download/chore/DefaultGroupTaskCall.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.imcys.bilibilias.core.datastore.preferences.AsPreferencesDataSource
 import com.imcys.bilibilias.core.download.task.GroupTask
 import com.imcys.bilibilias.core.ffmpeg.FFmpegWorker
+import com.imcys.bilibilias.core.ffmpeg.IFFmpegWork
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -11,19 +12,20 @@ class DefaultGroupTaskCall @Inject constructor(
     @ApplicationContext private val context: Context,
     private val fFmpegWorker: FFmpegWorker,
     private val userPreferences: AsPreferencesDataSource,
+    private val ifFmpegWork: IFFmpegWork,
 ) {
     fun execute(groupTask: GroupTask) {
-        if (groupTask.video.isCompelete && groupTask.audio.isCompelete) {
+//        if (groupTask.video.isCompeleted && groupTask.audio.isCompeleted) {
             try {
                 getResponseWithInterceptorChain(groupTask)
             } finally {
             }
-        }
+//        }
     }
 
     private fun getResponseWithInterceptorChain(groupTask: GroupTask) {
         val interceptors = mutableListOf<Interceptor<*>>()
-        interceptors += MixingInterceptor(context, fFmpegWorker, userPreferences)
+        interceptors += MixingInterceptor(context, userPreferences, ifFmpegWork)
         interceptors += MoveFileInterceptor(context, userPreferences)
         val chain = Interceptor.Chain(interceptors, 0)
         return chain.proceed(groupTask)
--- feature/download/build.gradle.kts
+++ feature/download/build.gradle.kts
@@ -1,4 +1,6 @@
-﻿plugins {
+﻿import com.android.build.api.dsl.Packaging
+
+plugins {
     alias(libs.plugins.bilibilias.android.feature)
     alias(libs.plugins.bilibilias.android.library.compose)
     alias(libs.plugins.bilibilias.android.library.jacoco)
@@ -6,6 +8,15 @@
 
 android {
     namespace = "com.imcys.bilibilias.feature.download"
+
+//    packaging {
+//        resources {
+//            pickFirsts += "lib/x86/libc++_shared.so"
+//            pickFirsts += "lib/x86_64/libc++_shared.so"
+//            pickFirsts += "lib/armeabi-v7a/libc++_shared.so"
+//            pickFirsts += "lib/arm64-v8a/libc++_shared.so"
+//        }
+//    }
 }
 
 dependencies {
