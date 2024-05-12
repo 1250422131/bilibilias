254971346426b5d2c2e2a4de898cb25c07404a77
sock_magic <startxingcheng@gmail.com>
2024-05-12 10:10:28 +0800

    feat: 播放模块

---
+++ feature/player/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.bilibilias.android.feature)
+    alias(libs.plugins.bilibilias.android.library.compose)
+    alias(libs.plugins.bilibilias.android.library.jacoco)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.core.player"
+}
+
+dependencies {
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+//    implementation("androidx.media3:media3-exoplayer-dash:1.3.1")
+
+    implementation(libs.akdanmaku)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
