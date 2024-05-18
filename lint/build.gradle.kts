818f68be5ddc3501369c6e39f6c2a6761f9ae539
sock_magic <startxingcheng@gmail.com>
2024-05-18 22:45:59 +0800

    更新
    Former-commit-id: b3a51f530bad30c5209ab3ed40b331c35f9d8eb0

---
+++ lint/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    kotlin("jvm")
+    alias(libs.plugins.bilibilias.android.lint)
+}
+
+java {
+    // Up to Java 11 APIs are available through desugaring
+    // https://developer.android.com/studio/write/java11-minimal-support-table
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.kotlin.stdlib)
+    compileOnly(libs.lint.api)
+    testImplementation(libs.lint.checks)
+    testImplementation(libs.lint.tests)
+    testImplementation(kotlin("test"))
+}
