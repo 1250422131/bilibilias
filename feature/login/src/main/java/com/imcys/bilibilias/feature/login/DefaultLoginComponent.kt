755161c395ff57c24254e5f92dc41a6ca8ae6bc7
sock_magic <startxingcheng@gmail.com>
2024-05-22 09:31:00 +0800

    refactor: 修复二维码文件未创建错误
    Former-commit-id: 87a9b04c3c92a25ec2bde80f4c89175c7f5f708c

--- feature/login/src/main/java/com/imcys/bilibilias/feature/login/DefaultLoginComponent.kt
+++ feature/login/src/main/java/com/imcys/bilibilias/feature/login/DefaultLoginComponent.kt
@@ -1,6 +1,11 @@
 package com.imcys.bilibilias.feature.login
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.arkivanov.decompose.ComponentContext
 import com.imcys.bilibilias.core.datastore.login.LoginInfoDataSource
 import com.imcys.bilibilias.core.network.repository.LoginRepository
@@ -8,7 +13,13 @@ import com.imcys.bilibilias.feature.common.AsComponentContext
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import io.github.alexzhirkevich.qrose.rememberQrCodePainter
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 class DefaultLoginComponent @AssistedInject constructor(
     @Assisted componentContext: ComponentContext,
@@ -27,4 +38,48 @@ class DefaultLoginComponent @AssistedInject constructor(
             componentContext: ComponentContext,
         ): DefaultLoginComponent
     }
+}
+
+@Composable
+private fun LoginPresenter(
+    events: Flow<LoginEvent>,
+    loginRepository: LoginRepository,
+    loginInfoDataSource: LoginInfoDataSource
+): LoginModel {
+    var qrUrl by remember { mutableStateOf("") }
+    var key by remember { mutableStateOf("") }
+    var isSuccess by remember { mutableStateOf(false) }
+    var message by remember { mutableStateOf("") }
+    LaunchedEffect(Unit) {
+        val (qrcodeKey, url) = loginRepository.获取二维码()
+        key = qrcodeKey
+        qrUrl = url
+    }
+
+    LaunchedEffect(key) {
+        var ok = false
+        withTimeout(3.minutes) {
+            while (!ok && isActive) {
+                delay(1.seconds)
+                val qrcodePoll = loginRepository.轮询登录(key)
+                ok = qrcodePoll.success
+                isSuccess = qrcodePoll.success
+                message = qrcodePoll.message
+                loginInfoDataSource.setRefreshToken(qrcodePoll.refreshToken)
+            }
+        }
+    }
+    LaunchedEffect(Unit) {
+        events.collect { event ->
+            when (event) {
+                LoginEvent.RefreshQrCode -> {
+                    val (qrcodeKey, url) = loginRepository.获取二维码()
+                    key = qrcodeKey
+                    qrUrl = url
+                }
+            }
+        }
+    }
+    val painter = rememberQrCodePainter(data = qrUrl)
+    return LoginModel(isSuccess, message, painter)
 }
