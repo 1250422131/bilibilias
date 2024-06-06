3117f1a36765b591083c693be8c5eee3b167ff8d
sock_magic <startxingcheng@gmail.com>
2024-06-06 20:59:20 +0800

    refactor: 更新
    Former-commit-id: 33b6c00d2994021f36dea65fdf063b5d6304a512

--- feature/authorspace/src/main/kotlin/com/imbys/bilibilias/feature/authorspace/AuthorSpaceComponent.kt
+++ feature/authorspace/src/main/kotlin/com/imbys/bilibilias/feature/authorspace/AuthorSpaceComponent.kt
@@ -2,11 +2,12 @@ package com.imbys.bilibilias.feature.authorspace
 
 import com.arkivanov.decompose.ComponentContext
 import com.imcys.bilibilias.core.model.video.Mid
+import kotlinx.coroutines.flow.StateFlow
 
 interface AuthorSpaceComponent {
-//    val models: StateFlow<Model>
-//    val dialogSlot: Value<ChildSlot<*, DialogComponent>>
+    val models: StateFlow<Model>
 
+    data class Model(val units: List<String>)
     interface Factory {
         operator fun invoke(
             componentContext: ComponentContext,
--- feature/authorspace/src/main/kotlin/com/imbys/bilibilias/feature/authorspace/DefaultAuthorSpaceComponent.kt
+++ feature/authorspace/src/main/kotlin/com/imbys/bilibilias/feature/authorspace/DefaultAuthorSpaceComponent.kt
@@ -3,6 +3,7 @@ package com.imbys.bilibilias.feature.authorspace
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.arkivanov.decompose.ComponentContext
+import com.imcys.bilibilias.core.model.video.Mid
 import com.imcys.bilibilias.core.network.repository.UserSpaceRepository
 import com.imcys.bilibilias.feature.common.BaseViewModel
 import dagger.assisted.Assisted
@@ -12,19 +13,23 @@ import kotlinx.coroutines.flow.Flow
 
 class DefaultAuthorSpaceComponent @AssistedInject constructor(
     @Assisted componentContext: ComponentContext,
+    @Assisted private val mid: Mid,
     private val userSpaceRepository: UserSpaceRepository,
-) : AuthorSpaceComponent, BaseViewModel<Unit, Unit>(componentContext) {
+) : AuthorSpaceComponent, BaseViewModel<Unit, AuthorSpaceComponent.Model>(componentContext) {
     @Composable
-    override fun models(events: Flow<Unit>) {
+    override fun models(events: Flow<Unit>) : AuthorSpaceComponent.Model {
         LaunchedEffect(Unit) {
-            userSpaceRepository.查询用户投稿视频(0)
+            userSpaceRepository.查询用户投稿视频(mid)
         }
+
+        return AuthorSpaceComponent.Model(emptyList())
     }
 
     @AssistedFactory
     interface Factory : AuthorSpaceComponent.Factory {
         override fun invoke(
             componentContext: ComponentContext,
+            mid: Mid,
         ): DefaultAuthorSpaceComponent
     }
 }
