66d2ec4d373ffb84355cb86ce1476556c31e7862
sock_magic <startxingcheng@gmail.com>
2024-06-13 01:14:54 +0800

    refactor: 更新

--- core/download/src/main/java/com/imcys/bilibilias/core/download/task/AudioTask.kt
+++ core/download/src/main/java/com/imcys/bilibilias/core/download/task/AudioTask.kt
@@ -1,22 +1,22 @@
 package com.imcys.bilibilias.core.download.task
 
-import com.imcys.bilibilias.core.download.DownloadRequest
 import com.imcys.bilibilias.core.model.download.FileType
 import com.imcys.bilibilias.core.model.video.VideoStreamUrl
 import com.imcys.bilibilias.core.model.video.ViewDetail
+import com.imcys.bilibilias.core.model.video.ViewInfo
 import java.io.File
 
 class AudioTask(
     streamUrl: VideoStreamUrl,
-    request: DownloadRequest,
+    info: ViewInfo,
     page: ViewDetail.Pages,
     path: String,
-) : AsDownloadTask(request.viewInfo, streamUrl, request, page.part) {
+) : AsDownloadTask(info, streamUrl, page.part) {
     override val priority = 100
     override val fileType = FileType.AUDIO
     override val destFile = File(path, "audio.aac")
     override val okTask = createTask(downloadUrl, destFile, priority)
-    override fun getStrategy(streamUrl: VideoStreamUrl, request: DownloadRequest): String {
+    override fun getStrategy(streamUrl: VideoStreamUrl): String {
         return streamUrl.dash.audio.maxBy { it.id }.baseUrl
     }
 }
