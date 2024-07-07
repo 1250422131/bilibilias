5fa4a4b1cfac89a145c786fad5f555ba2bd177ff
sock_magic <startxingcheng@gmail.com>
2024-07-07 20:52:04 +0800

    add module ffmpeg action

---
+++ feature/ffmpeg-action/README.md
@@ -0,0 +1,3 @@
+# :feature:ffmpeg-action module
+## Dependency graph
+![Dependency graph](../../docs/images/graphs/dep_graph_feature_ffmpeg_action.svg)
---
+++ feature/ffmpeg-action/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.bilibilias.android.feature)
+    alias(libs.plugins.bilibilias.android.compose)
+    alias(libs.plugins.bilibilias.android.jacoco)
+    alias(libs.plugins.bilibilias.multiplatform.decompose)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.feature.ffmpegaction"
+}
+
+dependencies {
+    testImplementation(projects.core.testing)
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+    androidTestImplementation(projects.core.testing)
+}
