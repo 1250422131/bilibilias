71dd322dba58f9dd78cc9647b7aa9b4a8c22feca
sock_magic <startxingcheng@gmail.com>
2024-07-09 22:37:07 +0800

    add test util
    Former-commit-id: eb11af4a2ab76d7fd77e19c4e636ae0c83c61ab6

---
+++ core/testing/src/main/kotlin/com/imcys/bilibilias/core/rules/GrantPostNotificationsPermissionRule.kt
@@ -0,0 +1,13 @@
+package com.imcys.bilibilias.core.rules
+
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.TIRAMISU
+import androidx.test.rule.GrantPermissionRule.grant
+import org.junit.rules.TestRule
+
+/**
+ * [TestRule] granting [POST_NOTIFICATIONS] permission if running on [SDK_INT] greater than [TIRAMISU].
+ */
+class GrantPostNotificationsPermissionRule :
+    TestRule by if (SDK_INT >= TIRAMISU) grant(POST_NOTIFICATIONS) else grant()
