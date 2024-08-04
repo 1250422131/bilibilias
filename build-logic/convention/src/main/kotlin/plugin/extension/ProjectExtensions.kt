7f937e3cf59be7fb9897e6d0ee8216faf392753e
sock_magic <startxingcheng@gmail.com>
2024-08-04 12:50:24 +0800

    清理文件

--- build-logic/convention/src/main/kotlin/plugin/extension/ProjectExtensions.kt
+++
@@ -1,9 +0,0 @@
-package plugin.extension
-
-import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.getByType
-
-val Project.libs
-    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
