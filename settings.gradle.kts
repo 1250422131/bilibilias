1547cb429273c8777b85fce64d1242bc5e4aed52
sock_magic <startxingcheng@gmail.com>
2024-08-06 18:10:48 +0800

    更新

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,7 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
