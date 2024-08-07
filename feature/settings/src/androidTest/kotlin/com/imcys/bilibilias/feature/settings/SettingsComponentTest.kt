9b608685af55ed0749e3aa273a5be868bd4fe031
sock_magic <startxingcheng@gmail.com>
2024-08-07 12:45:57 +0800

    chore: fix test code

--- feature/settings/src/androidTest/kotlin/com/imcys/bilibilias/feature/settings/SettingsComponentTest.kt
+++ feature/settings/src/androidTest/kotlin/com/imcys/bilibilias/feature/settings/SettingsComponentTest.kt
@@ -1,13 +1,9 @@
 package com.imcys.bilibilias.feature.settings
 
-import com.imcys.bilibilias.core.datastore.AsCookieStoreDataSource
-import com.imcys.bilibilias.core.datastore.AsPreferencesDataSource
-import com.imcys.bilibilias.feature.settings.component.SettingsComponent
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
-import javax.inject.Inject
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
@@ -16,14 +12,6 @@ class SettingsComponentTest {
 
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
-
-    private lateinit var component: SettingsComponent
-
-    @Inject
-    private lateinit var asPreferencesDataSource: AsPreferencesDataSource
-
-    @Inject
-    private lateinit var asCookieStoreDataSource: AsCookieStoreDataSource
 
     @BeforeTest
     fun setup() {
