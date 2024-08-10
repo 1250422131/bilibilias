998cffa828d4bc0cca2055b99ac106c3e9e2ab96
sock_magic <startxingcheng@gmail.com>
2024-08-10 11:54:59 +0800

    chore: 更新

---
+++ benchmarks/src/main/kotlin/com/imcys/bilibilias/baselineprofile/ToolBaselineProfile.kt
@@ -0,0 +1,17 @@
+package com.imcys.bilibilias.baselineprofile
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import com.imcys.bilibilias.PACKAGE_NAME
+import com.imcys.bilibilias.startActivityAndAllowNotifications
+import org.junit.Rule
+import org.junit.Test
+
+class ToolBaselineProfile {
+    @get:Rule val baselineProfileRule = BaselineProfileRule()
+
+    @Test
+    fun generate() =
+        baselineProfileRule.collect(PACKAGE_NAME) {
+            startActivityAndAllowNotifications()
+        }
+}
