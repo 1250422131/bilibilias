7bae8b1a44c143626257aecd068791549f830736
sock_magic <startxingcheng@gmail.com>
2024-08-16 21:21:06 +0800

    add module media
    Former-commit-id: 1575977a17763cb1cf00f9d2f89e0088282d8092

---
+++ core/media/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.compose)
+    alias(libs.plugins.bilibilias.android.jacoco)
+}
+
+android {
+    namespace = "com.imcys.bilibilias.core.media"
+}
+
+dependencies {
+    api(libs.androidx.metrics)
+    api(projects.core.analytics)
+    api(projects.core.designsystem)
+    api(projects.core.model)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+    implementation(libs.coil.video)
+    implementation(libs.coil.svg)
+
+    androidTestImplementation(projects.core.testing)
+    androidTestImplementation(testFixtures(projects.core.model))
+}
