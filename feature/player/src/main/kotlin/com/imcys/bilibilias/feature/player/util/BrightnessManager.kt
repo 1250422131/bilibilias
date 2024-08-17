a3923da18bdf008642f15319aab0ae7903fc7936
sock_magic <startxingcheng@gmail.com>
2024-08-17 22:07:01 +0800

    update

---
+++ feature/player/src/main/kotlin/com/imcys/bilibilias/feature/player/util/BrightnessManager.kt
@@ -0,0 +1,24 @@
+package com.imcys.bilibilias.feature.player.util
+
+import android.view.WindowManager
+import com.imcys.bilibilias.feature.player.PlayerActivity
+import com.imcys.bilibilias.feature.player.extensions.currentBrightness
+import com.imcys.bilibilias.feature.player.extensions.swipeToShowStatusBars
+
+class BrightnessManager(private val activity: PlayerActivity) {
+
+    var currentBrightness = activity.currentBrightness
+    val maxBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL
+
+    val brightnessPercentage get() = (currentBrightness / maxBrightness).times(100).toInt()
+
+    fun setBrightness(brightness: Float) {
+        currentBrightness = brightness.coerceIn(0f, maxBrightness)
+        val layoutParams = activity.window.attributes
+        layoutParams.screenBrightness = currentBrightness
+        activity.window.attributes = layoutParams
+
+        // fixes a bug which makes the action bar reappear after changing the brightness
+        activity.swipeToShowStatusBars()
+    }
+}
