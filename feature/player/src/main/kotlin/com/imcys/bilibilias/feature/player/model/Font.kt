74ad1247c5f4fc657a1acd970eb49fb7244dd65d
sock_magic <startxingcheng@gmail.com>
2024-08-17 21:59:54 +0800

    update

---
+++ feature/player/src/main/kotlin/com/imcys/bilibilias/feature/player/model/Font.kt
@@ -0,0 +1,8 @@
+package com.imcys.bilibilias.feature.player.model
+
+enum class Font {
+    DEFAULT,
+    MONOSPACE,
+    SANS_SERIF,
+    SERIF,
+}
---
+++ feature/player/src/main/kotlin/com/imcys/bilibilias/feature/player/model/ScreenOrientation.kt
@@ -0,0 +1,10 @@
+package com.imcys.bilibilias.feature.player.model;
+
+enum class ScreenOrientation {
+    AUTOMATIC,
+    LANDSCAPE,
+    LANDSCAPE_REVERSE,
+    LANDSCAPE_AUTO,
+    PORTRAIT,
+    VIDEO_ORIENTATION,
+}
