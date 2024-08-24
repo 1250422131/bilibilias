9c40f8c248f174f62b242fa082cf89c17f85e904
sock_magic <startxingcheng@gmail.com>
2024-08-24 11:17:42 +0800

    refactor: Integrate compose compiler metrics and reports

--- build-logic/convention/src/main/kotlin/com/imcys/bilibilias/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/com/imcys/bilibilias/ProjectExtensions.kt
@@ -3,6 +3,8 @@ package com.imcys.bilibilias
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.file.Directory
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.getByType
 
@@ -12,3 +14,16 @@ val Project.libs
 fun Project.applyPlugin(alias: String) {
     apply(plugin = libs.findPlugin(alias).get().get().pluginId)
 }
+
+/**
+ * Create new Directory by [dir] name in the root build directory.
+ */
+internal fun Project.relativeToRootProject(dir: String): Provider<Directory> =
+    rootProject.layout.buildDirectory.dir(projectDir.toRelativeString(rootDir))
+        .map { it.dir(dir) }
+
+/**
+ * Check a [propertyName]'s property value is true.
+ */
+internal fun Project.isPropertyValueIsTrue(propertyName: String): Boolean =
+    properties[propertyName].toString().toBoolean()
