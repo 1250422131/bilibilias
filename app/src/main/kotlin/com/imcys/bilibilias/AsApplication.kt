eca39f64e30f80f3b294b041dbfe88bf216b9e90
sock_magic <startxingcheng@gmail.com>
2024-08-25 01:08:19 +0800

    refactor: update

--- app/src/main/kotlin/com/imcys/bilibilias/AsApplication.kt
+++ app/src/main/kotlin/com/imcys/bilibilias/AsApplication.kt
@@ -24,7 +24,7 @@ class AsApplication :
     override fun onCreate() {
         super.onCreate()
         Napier.base(DebugAntilog())
-        logging(this)
+        logging()
         Sync.initialize(context = this)
     }
 
