19ea1f6f23ad69ca958ccc0e2a3fc6a54ba2c2d3
sock_magic <startxingcheng@gmail.com>
2024-09-06 16:58:25 +0800

    feat: 下载任务通知
    Former-commit-id: 5adbab64ed8a19e499d3a2a700e6e672f43b0d27

--- core/notifications/src/main/kotlin/com/imcys/bilibilias/core/notifications/NoOpNotifier.kt
+++ core/notifications/src/main/kotlin/com/imcys/bilibilias/core/notifications/NoOpNotifier.kt
@@ -1,10 +1,11 @@
 package com.imcys.bilibilias.core.notifications
 
+import com.imcys.bilibilias.core.model.download.DownloadTask
 import javax.inject.Inject
 
 /**
  * Implementation of [Notifier] which does nothing. Useful for tests and previews.
  */
 internal class NoOpNotifier @Inject constructor() : Notifier {
-    override fun postNewsNotifications(newsResources: List<*>) = Unit
+    override fun postNewsNotifications(tasks: List<DownloadTask>) = Unit
 }
