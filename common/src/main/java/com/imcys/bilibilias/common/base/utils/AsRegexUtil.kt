6cd9d802c7c1d8c9e2f94f082f42b4f7fe6ae88b
萌新杰少 <1250422131@qq.com>
2025-01-04 18:38:12 +0800

    支持bili2233短链
    Former-commit-id: d9b856174e3b95734f6bf786c53c7b0357df48d9

--- common/src/main/java/com/imcys/bilibilias/common/base/utils/AsRegexUtil.kt
+++ common/src/main/java/com/imcys/bilibilias/common/base/utils/AsRegexUtil.kt
@@ -6,10 +6,12 @@ object AsRegexUtil {
     private val regex3 = Regex("""https://space.bilibili.com/?(\d+).*""")
     private val regex4 = Regex("""(?<=BV|Bv|bv|bV)[A-Za-z0-9]{10}""")
     private val regex5 = Regex("""(?<=(av|aV|AV|Av))([0-9]+)""")
+    private val regex6 = Regex("""https://bili2233.cn/(([A-z]+|\d+){6,})""")
     private fun isBV(text: String) = regex4.containsMatchIn(text)
     private fun isAV(text: String) = regex5.containsMatchIn(text)
     private fun isEP(text: String) = regex1.containsMatchIn(text)
     private fun isShortLink(text: String) = text.contains("""https://b23.tv""")
+    private fun is2233ShortLink(text: String) = text.contains("""https://bili2233.cn""")
     private fun isUserSpace(text: String) = text.startsWith("""https://space.bilibili.com""")
     fun parse(text: String): TextType? {
         return when {
@@ -17,6 +19,7 @@ object AsRegexUtil {
             isAV(text) -> regex5.find(text)?.groupValues?.get(2)?.let { TextType.AV(it.toLong()) }
             isEP(text) -> regex1.find(text)?.groupValues?.get(1)?.let { TextType.EP(it.toLong()) }
             isShortLink(text) -> regex2.find(text)?.groupValues?.get(1)?.let { TextType.ShortLink("https://b23.tv/$it") }
+            is2233ShortLink(text) -> regex6.find(text)?.groupValues?.get(1)?.let { TextType.ShortLink("https://space.bilibili.com/$it") }
             isUserSpace(text) -> regex3.find(text)?.groupValues?.get(1)?.let { TextType.UserSpace(it) }
             else -> null
         }
