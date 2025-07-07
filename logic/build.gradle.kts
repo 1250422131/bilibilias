744914bcb4e49045bd3ce8b274a0ddc48764ed7c
sock_magic <startxingcheng@gmail.com>
2025-07-07 19:05:30 +0800

    Refactor: Update dependencies in logic module
    This commit updates the dependencies for the `logic` module.
    
    Specific changes:
    - In `logic/build.gradle.kts`:
        - Added `projects.core.datasource` as an API dependency to `commonMain`.
    
    
    Former-commit-id: 8130bd0cc5ebd091d8a9fb112db864e934952fe0

--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -7,6 +7,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             api(projects.core.common)
+            api(projects.core.datasource)
 
             api(libs.decompose)
         }
