8b6c27ff5fe9ee2c5386bb499509e1479bd887f5
sock_magic <startxingcheng@gmail.com>
2025-07-08 23:31:46 +0800

    Refactor: Update datasource dependencies
    This commit updates the dependencies in `core/datasource/build.gradle.kts`.
    
    Specific changes:
    - Removed direct Ktor client dependencies:
        - `libs.ktor.client.core`
        - `libs.ktor.client.content.negotiation`
        - `libs.ktor.serialization.kotlinx.json`
        - `libs.ktor.client.logging`
        - `libs.ktor.client.okhttp` (from `jvmMain`)
    - Added `projects.core.ktorClient` as a dependency in `commonMain`.
    
    
    Former-commit-id: 6e36c2a62571fefd4707e31178174649ef209079

--- core/datasource/build.gradle.kts
+++ core/datasource/build.gradle.kts
@@ -7,14 +7,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.core.common)
-
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.ktor.client.logging)
-        }
-        jvmMain.dependencies {
-            implementation(libs.ktor.client.okhttp)
+            implementation(projects.core.ktorClient)
         }
     }
 }
