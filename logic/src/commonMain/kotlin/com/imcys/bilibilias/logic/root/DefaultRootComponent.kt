49d29d78fe113940a079d9a8bdd6958b198a5efd
sock_magic <startxingcheng@gmail.com>
2025-07-11 17:38:51 +0800

    feat: Add CacheComponent and navigation logic
    This commit introduces a `CacheComponent` and updates the `RootComponent` to handle navigation to the new cache screen.
    
    Specific changes:
    - In `RootComponent.kt`:
        - Added `onCacheClicked()` function to the `RootComponent` interface.
        - Added `CacheChild` to the `Child` sealed class, taking a `CacheComponent`.
    - In `DefaultRootComponent.kt`:
        - Modified `child()` function to create `DefaultCacheComponent` when `Config.Cache` is active.
        - Implemented `onSearchClicked()` to bring the `Search` configuration to the front of the navigation stack.
        - Implemented `onCacheClicked()` to bring the `Cache` configuration to the front of the navigation stack.
        - Added `Cache` data object to the `Config` sealed interface.
    
    
    Former-commit-id: d79960542dfc839b5d982baf9a8451fa6300c3fe

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/DefaultRootComponent.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/DefaultRootComponent.kt
@@ -3,9 +3,11 @@ package com.imcys.bilibilias.logic.root
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.bringToFront
 import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.value.Value
+import com.imcys.bilibilias.logic.cache.DefaultCacheComponent
 import com.imcys.bilibilias.logic.search.DefaultSearchComponent
 import kotlinx.serialization.Serializable
 
@@ -15,8 +17,7 @@ class DefaultRootComponent(
 
     private val nav = StackNavigation<Config>()
 
-    private val _stack =
-        childStack(
+    private val _stack = childStack(
             source = nav,
             serializer = Config.serializer(),
             childFactory = ::child,
@@ -28,15 +29,27 @@ class DefaultRootComponent(
     private fun child(config: Config, componentContext: ComponentContext): RootComponent.Child =
         when (config) {
             Config.Search -> RootComponent.Child.SearchChild(DefaultSearchComponent(componentContext))
+            Config.Cache -> RootComponent.Child.SearchChild(DefaultCacheComponent(componentContext))
         }
 
     override fun onBackClicked() {
         nav.pop()
     }
 
+    override fun onSearchClicked() {
+        nav.bringToFront(Config.Search)
+    }
+
+    override fun onCacheClicked() {
+        nav.bringToFront(Config.Cache)
+    }
+
     @Serializable
     private sealed interface Config {
         @Serializable
         data object Search : Config
+
+        @Serializable
+        data object Cache : Config
     }
 }
--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/RootComponent.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/RootComponent.kt
@@ -3,15 +3,18 @@ package com.imcys.bilibilias.logic.root
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.essenty.backhandler.BackHandlerOwner
+import com.imcys.bilibilias.logic.cache.CacheComponent
 import com.imcys.bilibilias.logic.search.SearchComponent
 
 interface RootComponent : BackHandlerOwner {
 
     val stack: Value<ChildStack<*, Child>>
-
+    fun onSearchClicked()
+    fun onCacheClicked()
     fun onBackClicked()
 
     sealed class Child {
         data class SearchChild(val component: SearchComponent) : Child()
+        data class CacheChild(val component: CacheComponent) : Child()
     }
 }
