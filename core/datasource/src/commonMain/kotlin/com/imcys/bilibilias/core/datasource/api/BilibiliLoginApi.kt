13b8d59f9b75b6939cd0d8ad0716840109428778
sock_magic <startxingcheng@gmail.com>
2025-07-15 21:11:26 +0800

    Refactor: Update pollRequest to include qrcode_key parameter
    This commit modifies the `pollRequest` function in `BilibiliLoginApi.kt` to accept a `key` parameter, which is then used as the `qrcode_key` in the API call.
    
    Specific changes:
    - In `core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/api/BilibiliLoginApi.kt`:
        - Updated the `pollRequest` function signature to include `key: String`.
        - Added `parameter("qrcode_key", key)` to the `client.get` call within `pollRequest`.
    
    
    Former-commit-id: 62f88ee2d3f16b401ee9c3c3bc66917778360860

--- core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/api/BilibiliLoginApi.kt
+++ core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/api/BilibiliLoginApi.kt
@@ -15,6 +15,7 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.serialization.kotlinx.json.json
 
 object BilibiliLoginApi : AutoCloseable {
@@ -44,8 +45,10 @@ object BilibiliLoginApi : AutoCloseable {
         return client.get("/x/passport-login/web/qrcode/generate").body<QrCode>()
     }
 
-    suspend fun pollRequest(): PollResponse {
-        return client.get("/x/passport-login/web/qrcode/poll").body<PollResponse>()
+    suspend fun pollRequest(key: String): PollResponse {
+        return client.get("/x/passport-login/web/qrcode/poll") {
+            parameter("qrcode_key", key)
+        }.body<PollResponse>()
     }
 
     override fun close() {
