cbbaee1b57c9634d8608e5b66100dec1e0cc0360
sock_magic <startxingcheng@gmail.com>
2025-07-15 21:12:02 +0800

    feat: Add TokenPersistent for managing refresh tokens
    This commit introduces `TokenPersistent`, a DataStore-based mechanism for persisting and retrieving refresh tokens.
    
    Specific changes:
    - Created `TokenPersistent.kt` in `core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/persistent`.
    - Defined `TokenPersistent` object which utilizes `DataStore` to store `TokenSave` objects.
        - Uses `TokenSave.serializer().asDataStoreSerializer` for serialization.
        - Implements `ReplaceFileCorruptionHandler` to handle data corruption.
        - Stores data in a file named "token" using `resolveDataStoreFile`.
    - Exposed `refreshToken` as a `Flow<String?>` to observe token changes.
    - Provided `setRefreshToken(value: String)` suspend function to update the stored refresh token.
    - Created `TokenSave` serializable data class to hold the `refreshToken`.
        - Includes a companion object with an `INIT` state for initial/empty data.
    
    
    Former-commit-id: 9e6201484ef088f04e510889adebeafc4df2d230

---
+++ core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/persistent/TokenPersistent.kt
@@ -0,0 +1,37 @@
+package com.imcys.bilibilias.core.datasource.persistent
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import com.imcys.bilibilias.core.datasource.persistent.TokenSave.Companion.INIT
+import com.imcys.bilibilias.core.datastore.asDataStoreSerializer
+import com.imcys.bilibilias.core.datastore.new
+import com.imcys.bilibilias.core.datastore.resolveDataStoreFile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.Serializable
+
+object TokenPersistent {
+    private val dataStore: DataStore<TokenSave> = DataStoreFactory.new(
+        serializer = TokenSave.serializer().asDataStoreSerializer { INIT },
+        corruptionHandler = ReplaceFileCorruptionHandler { INIT },
+        produceFile = { resolveDataStoreFile("token") }
+    )
+
+    val refreshToken: Flow<String?> = dataStore.data.map { it.refreshToken }
+    suspend fun setRefreshToken(value: String) {
+        dataStore.updateData {
+            it.copy(refreshToken = value)
+        }
+    }
+}
+
+@ConsistentCopyVisibility
+@Serializable
+data class TokenSave internal constructor(
+    val refreshToken: String? = null,
+) {
+    companion object {
+        val INIT = TokenSave()
+    }
+}
