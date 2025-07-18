0c4063cc724e69b11c784be0a93bf40a162ff521
sock_magic <startxingcheng@gmail.com>
2025-07-18 20:46:03 +0800

    Refactor: Update resource imports in App.kt
    This commit updates the import paths for resources in `App.kt`.
    
    Specific changes:
    - Changed `bilibilias.composeapp.generated.resources.Res` to `bilibilias.app.generated.resources.Res`.
    - Changed `bilibilias.composeapp.generated.resources.compose_multiplatform` to `bilibilias.app.generated.resources.compose_multiplatform`.
    
    
    Former-commit-id: 8b149afea8ca0838dfd081ce42f38f1356660df1

--- app/src/commonMain/kotlin/com/imcys/bilibilias/App.kt
+++ app/src/commonMain/kotlin/com/imcys/bilibilias/App.kt
@@ -9,11 +9,15 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import bilibilias.composeapp.generated.resources.Res
-import bilibilias.composeapp.generated.resources.compose_multiplatform
+import bilibilias.app.generated.resources.Res
+import bilibilias.app.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
