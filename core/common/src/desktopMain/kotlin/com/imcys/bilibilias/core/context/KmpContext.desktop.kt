f0ca045439ec01773199684ba981eb595f4209e2
sock_magic <startxingcheng@gmail.com>
2025-07-18 20:38:10 +0800

    feat: Add initial Desktop platform context implementation
    This commit introduces the initial implementation of `KmpContext` and `LocalKmpContext` for the Desktop platform.
    
    Specific changes:
    - Created `core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/context/KmpContext.desktop.kt` with placeholder implementations for:
        - `isDebug`
        - `cacheDir`
        - `dataDir`
        - `dataStoreDir`
        - `logsDir`
        - `platform`
    - Created `core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/context/LocalKmpContext.desktop.kt` with a placeholder implementation for `LocalKmpContext`.
    
    
    Former-commit-id: e18156ad184119988471975aa33481c3c9e4a396

---
+++ core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/context/KmpContext.desktop.kt
@@ -0,0 +1,16 @@
+package com.imcys.bilibilias.core.context
+
+actual object KmpContext {
+    actual val isDebug: Boolean
+        get() = TODO("Not yet implemented")
+    actual val cacheDir: kotlinx.io.files.Path
+        get() = TODO("Not yet implemented")
+    actual val dataDir: kotlinx.io.files.Path
+        get() = TODO("Not yet implemented")
+    actual val dataStoreDir: kotlinx.io.files.Path
+        get() = TODO("Not yet implemented")
+    actual val logsDir: kotlinx.io.files.Path
+        get() = TODO("Not yet implemented")
+    actual val platform: com.imcys.bilibilias.core.context.Platform
+        get() = TODO("Not yet implemented")
+}
---
+++ core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/context/LocalKmpContext.desktop.kt
@@ -0,0 +1,6 @@
+package com.imcys.bilibilias.core.context
+
+import androidx.compose.runtime.ProvidableCompositionLocal
+
+actual val LocalKmpContext: ProvidableCompositionLocal<KmpContext>
+    get() = TODO("Not yet implemented")
