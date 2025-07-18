bc09ff6320747bb8961e5a96599c3a6e59ef6add
sock_magic <startxingcheng@gmail.com>
2025-07-18 20:38:59 +0800

    feat: Implement desktop DataStore file resolution
    This commit introduces the desktop-specific implementation for resolving DataStore file paths.
    
    Specific changes:
    - Added `core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/datastore/DataStore.desktop.kt`.
    - Implemented the `resolveDataStoreFile` function for the desktop platform, which currently contains a `TODO` for future implementation.
    
    
    Former-commit-id: f798fb9a6f299c2590ae9e01b0d8d43cc4b9f71c

---
+++ core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/datastore/DataStore.desktop.kt
@@ -0,0 +1,7 @@
+package com.imcys.bilibilias.core.datastore
+
+import com.imcys.bilibilias.core.io.SystemPath
+
+actual fun resolveDataStoreFile(name: String): SystemPath {
+    TODO("Not yet implemented")
+}
