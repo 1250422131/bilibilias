226912af19d7e8c8df2e8283c3eecc15f4083279
sock_magic <startxingcheng@gmail.com>
2025-07-19 22:02:13 +0800

    Refactor: Remove HttpMediaCacheCenter
    This commit removes the `HttpMediaCacheCenter.kt` file and the `HttpMediaCacheCenter` class.
    
    Specific changes:
    - Deleted `logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/HttpMediaCacheCenter.kt`.
    
    
    Former-commit-id: ee98c713374e007c55d4dc7c4ad6a85ded57ec33

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/HttpMediaCacheCenter.kt
+++
@@ -1,9 +0,0 @@
-package com.imcys.bilibilias.logic
-
-import com.imcys.bilibilias.core.http.downloader.HttpDownloader
-
-class HttpMediaCacheCenter(
-    private val httpDownloader: HttpDownloader
-) {
-//    val s = httpDownloader.getState()
-}
