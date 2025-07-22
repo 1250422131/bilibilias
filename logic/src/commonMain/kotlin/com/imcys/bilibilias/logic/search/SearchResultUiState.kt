1c3985ab0530405400638cdfc0f62836a8c7dbaa
sock_magic <startxingcheng@gmail.com>
2025-07-22 20:47:52 +0800

    Refactor: Update SearchResultUiState.Success with new episode fields
    This commit modifies the `Success` data class within the `SearchResultUiState` sealed interface.
    
    Specific changes:
    - Added `episodeInfo: EpisodeInfo2` and `episodes: List<EpisodeCacheState>` properties to the `SearchResultUiState.Success` data class.
    - Imported `EpisodeCacheState` and `EpisodeInfo2` from `com.imcys.bilibilias.core.data.model`.
    
    
    Former-commit-id: 9b557bc39567e86b7479339838fdc457e1bd5c97

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/search/SearchResultUiState.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/search/SearchResultUiState.kt
@@ -1,6 +1,8 @@
 package com.imcys.bilibilias.logic.search
 
 import com.imcys.bilibilias.core.data.model.EpisodeCacheListState
+import com.imcys.bilibilias.core.data.model.EpisodeCacheState
+import com.imcys.bilibilias.core.data.model.EpisodeInfo2
 
 sealed interface SearchResultUiState {
     data object Loading : SearchResultUiState
@@ -15,6 +17,8 @@ sealed interface SearchResultUiState {
     data object LoadFailed : SearchResultUiState
 
     data class Success(
+        val episodeInfo: EpisodeInfo2,
+        val episodes: List<EpisodeCacheState>,
         val episodeCacheListState: EpisodeCacheListState,
     ) : SearchResultUiState
 
