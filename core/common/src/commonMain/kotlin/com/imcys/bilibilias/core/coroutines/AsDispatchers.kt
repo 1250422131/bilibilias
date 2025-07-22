18b87dd14ac15fc8ed36cf63422b53b2e0d2f56d
sock_magic <startxingcheng@gmail.com>
2025-07-22 22:42:44 +0800

    Refactor: Add CoroutineExceptionHandler to ApplicationScope
    This commit updates the `ApplicationScope` in `AsDispatchers.kt` to include a `CoroutineExceptionHandler`. This handler will log any uncaught exceptions that occur within coroutines launched in this scope using `Logger.w`.
    
    Specific changes:
    - Modified `AsDispatchers.kt`:
        - Added a `CoroutineExceptionHandler` to the `applicationScope`.
        - The exception handler logs the throwable and the coroutine context using `Logger.w`.
        - Renamed `ApplicationScope` to `applicationScope` to follow Kotlin's naming conventions for properties.
    
    
    Former-commit-id: de1390182372a6ee8f7e7ac1fd2f7d2597c564aa

--- core/common/src/commonMain/kotlin/com/imcys/bilibilias/core/coroutines/AsDispatchers.kt
+++ core/common/src/commonMain/kotlin/com/imcys/bilibilias/core/coroutines/AsDispatchers.kt
@@ -1,12 +1,20 @@
 package com.imcys.bilibilias.core.coroutines
 
+import co.touchlab.kermit.Logger
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 
 object AsDispatchers {
-    val ApplicationScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    val applicationScope: CoroutineScope = CoroutineScope(
+        CoroutineExceptionHandler { coroutineContext, throwable ->
+            Logger.w("ApplicationScope", throwable) {
+                "Uncaught exception in coroutine $coroutineContext"
+            }
+        } + SupervisorJob() + Dispatchers.Default,
+    )
     val IO: CoroutineDispatcher = Dispatchers.IO
     val Default: CoroutineDispatcher = Dispatchers.Default
 }
