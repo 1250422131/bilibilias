0e5eaa638fa5eb833e787669041de131446d0904
sock_magic <startxingcheng@gmail.com>
2025-07-24 20:23:30 +0800

    Refactor: Update ffmpeg module dependencies
    This commit updates the dependencies for the `core/ffmpeg` module.
    
    Specific changes:
    - In `core/ffmpeg/build.gradle.kts`:
        - Added `implementation(projects.core.common)` to the `commonMain` source set dependencies.
    
    
    Former-commit-id: eb3a16297f12590dce9b35c23679979ed88c47af

--- core/ffmpeg/build.gradle.kts
+++ core/ffmpeg/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 
 kotlin {
     sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.common)
+        }
         jvmMain.dependencies {
             implementation("org.bytedeco:ffmpeg-platform:7.1.1-1.5.12")
             implementation("com.github.pgreze:kotlin-process:1.5.1")
