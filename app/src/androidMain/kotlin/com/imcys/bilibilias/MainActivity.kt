37633eff8aaa0512d739d6460c0208c6e1c9a7aa
sock_magic <startxingcheng@gmail.com>
2025-07-24 21:48:24 +0800

    Refactor: Introduce LifecycleOwner and update RootComponent initialization
    This commit introduces `LifecycleOwner` and updates the initialization of `DefaultRootComponent` in `MainActivity.kt`.
    
    Specific changes:
    - Created a `LifecycleOwner` using `essentyLifecycle()`.
    - Updated `DefaultRootComponent` initialization to use `DefaultAppComponentContext(defaultComponentContext())`.
    - Provided the new `lifecycleOwner` through `LocalLifecycleOwner` using `CompositionLocalProvider`.
    
    
    Former-commit-id: 612501439f3e22fcd7cd122cefbfa2d49f1b5a3e

--- app/src/androidMain/kotlin/com/imcys/bilibilias/MainActivity.kt
+++ app/src/androidMain/kotlin/com/imcys/bilibilias/MainActivity.kt
@@ -8,19 +8,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
 import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleOwner
+import com.arkivanov.essenty.lifecycle.essentyLifecycle
 import com.imcys.bilibilias.core.context.KmpContext
 import com.imcys.bilibilias.core.context.LocalKmpContext
+import com.imcys.bilibilias.logic.root.DefaultAppComponentContext
 import com.imcys.bilibilias.logic.root.DefaultRootComponent
+import com.imcys.bilibilias.ui.LocalLifecycleOwner
 import com.imcys.bilibilias.ui.root.AsApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        val root = DefaultRootComponent(defaultComponentContext())
+
+        val root = DefaultRootComponent(DefaultAppComponentContext(defaultComponentContext()))
+        val lifecycleOwner = object : LifecycleOwner {
+            override val lifecycle get() = essentyLifecycle()
+        }
         setContent {
             CompositionLocalProvider(
-                LocalKmpContext provides KmpContext
+                LocalKmpContext provides KmpContext,
+                LocalLifecycleOwner provides lifecycleOwner,
             ) {
                 AsApp(root)
             }
