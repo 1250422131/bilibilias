6689117fd9f67dba49160f7e403f27fa5338c918
sock_magic <startxingcheng@gmail.com>
2025-07-28 16:14:34 +0800

    Feat: Add global uncaught exception handler
    This commit introduces a global uncaught exception handler in `AsApplication`.
    
    Specific changes:
    - In `AsApplication.kt`:
        - Added an uncaught exception handler that logs fatal errors using `co.touchlab.kermit.Logger`.
        - The original default uncaught exception handler is preserved and called after logging.
    
    
    Former-commit-id: 446c703bf6dd9996c5051f531f6e494bd8bccbd3

--- app/src/androidMain/kotlin/com/imcys/bilibilias/AsApplication.kt
+++ app/src/androidMain/kotlin/com/imcys/bilibilias/AsApplication.kt
@@ -1,6 +1,7 @@
 package com.imcys.bilibilias
 
 import android.app.Application
+import co.touchlab.kermit.Logger
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
@@ -12,6 +13,12 @@ import com.imcys.bilibilias.core.ktor.client.createHttpClient
 class AsApplication : Application(), SingletonImageLoader.Factory {
     override fun onCreate() {
         super.onCreate()
+
+        val defaultUEH = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            Logger.e("AsApplication", e, "!!! FATAL !!!")
+            defaultUEH?.uncaughtException(t, e)
+        }
     }
 
     override fun newImageLoader(context: PlatformContext): ImageLoader {
