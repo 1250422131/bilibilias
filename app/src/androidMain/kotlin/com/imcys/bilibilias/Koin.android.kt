9e1054a0a55fcf6894a20667c5d7f0df7b01daf9
sock_magic <startxingcheng@gmail.com>
2025-07-29 22:43:34 +0800

    Refactor: Update AppComponentContext to use KmpContext provider
    This commit modifies the initialization of `DefaultAppComponentContext` within the Koin dependency injection setup. Instead of directly using the `KmpContext` object, it now resolves `KmpContext` through Koin's `get()` mechanism.
    
    Specific changes:
    - In `app/src/androidMain/kotlin/com/imcys/bilibilias/Koin.android.kt`:
        - Changed `KmpContext` to `get()` in the `DefaultAppComponentContext` constructor call.
    
    
    Former-commit-id: b4a1d6fe2ece14a73793d296d3ce28c592607953

--- app/src/androidMain/kotlin/com/imcys/bilibilias/Koin.android.kt
+++ app/src/androidMain/kotlin/com/imcys/bilibilias/Koin.android.kt
@@ -2,7 +2,6 @@ package com.imcys.bilibilias
 
 import androidx.activity.ComponentActivity
 import com.arkivanov.decompose.defaultComponentContext
-import com.imcys.bilibilias.core.context.KmpContext
 import com.imcys.bilibilias.logic.root.AppComponentContext
 import com.imcys.bilibilias.logic.root.DefaultAppComponentContext
 import org.koin.core.module.Module
@@ -12,7 +11,7 @@ actual val PlatformModule: Module = module {
     single<AppComponentContext> {
         DefaultAppComponentContext(
             get<ComponentActivity>().defaultComponentContext(),
-            KmpContext,
+            get(),
             get()
         )
     }
