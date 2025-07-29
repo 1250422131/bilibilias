4d7baf4b6441ca44c1acdb1d823769e6f15c8bba
sock_magic <startxingcheng@gmail.com>
2025-07-29 10:53:03 +0800

    Refactor: Remove unused App, Greeting, and Platform files
    This commit removes several files that are no longer in use:
    - `App.kt`
    - `Greeting.kt`
    - `Platform.android.kt`
    - `Platform.kt`
    
    
    Former-commit-id: 587f02d004169f2f7d66061b9eb6607408d64bf8

--- app/src/commonMain/kotlin/com/imcys/bilibilias/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.imcys.bilibilias
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- app/src/commonMain/kotlin/com/imcys/bilibilias/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.imcys.bilibilias
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
