00305b5713931d0b430359d96a2cc24b03ff9c78
sock_magic <startxingcheng@gmail.com>
2025-07-29 21:52:07 +0800

    Refactor: Remove unused coroutineScopeModule
    This commit removes the `coroutineScopeModule` from `CoroutineScopeModule.kt` as it is no longer used.
    
    Specific changes:
    - Deleted the `coroutineScopeModule` `val` and its associated `module` block.
    - Removed unused imports for `co.touchlab.kermit.Logger`, `kotlinx.coroutines.CoroutineExceptionHandler`, `kotlinx.coroutines.SupervisorJob`, and `org.koin.dsl.module`.
    
    
    Former-commit-id: 8e9ad9aabc0b4fa23e73629e09ed755943fe8679

--- core/common/src/commonMain/kotlin/com/imcys/bilibilias/core/di/CoroutineScopeModule.kt
+++ core/common/src/commonMain/kotlin/com/imcys/bilibilias/core/di/CoroutineScopeModule.kt
@@ -1,23 +1,7 @@
 package com.imcys.bilibilias.core.di
 
-import co.touchlab.kermit.Logger
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import org.koin.core.scope.Scope
-import org.koin.dsl.module
-
-val coroutineScopeModule = module {
-    single {
-        CoroutineScope(
-            CoroutineExceptionHandler { coroutineContext, throwable ->
-                Logger.w("ApplicationScope", throwable) {
-                    "Uncaught exception in coroutine $coroutineContext"
-                }
-            } + SupervisorJob() + IO,
-        )
-    }
-}
 
 val Scope.applicationScope: CoroutineScope
     get() = get()
