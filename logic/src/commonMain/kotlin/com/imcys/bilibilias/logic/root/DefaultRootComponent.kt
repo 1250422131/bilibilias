c92365eecf56c508546bb19edb8ffaf019e4dfdb
sock_magic <startxingcheng@gmail.com>
2025-07-29 22:00:14 +0800

    Refactor: Inject components in DefaultRootComponent
    This commit modifies `DefaultRootComponent` to use Koin for dependency injection of its child components.
    
    Specific changes:
    - Added `KoinComponent` interface to `DefaultRootComponent`.
    - Injected `SearchComponent`, `CacheComponent`, `LoginComponent`, `PlayerComponent`, and `SettingsComponent` using `inject()`.
    - Removed manual instantiation of default component implementations (e.g., `DefaultSearchComponent(componentContext)`) in the `child` function and replaced them with the injected instances.
    
    
    Former-commit-id: 0e87725b1567e13d34ad9d6d3ab56be3f1fe3fa3

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/DefaultRootComponent.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/DefaultRootComponent.kt
@@ -7,22 +7,27 @@ import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.pushNew
 import com.arkivanov.decompose.value.Value
-import com.imcys.bilibilias.logic.cache.DefaultCacheComponent
-import com.imcys.bilibilias.logic.login.DefaultLoginComponent
-import com.imcys.bilibilias.logic.player.DefaultPlayerComponent
+import com.imcys.bilibilias.logic.cache.CacheComponent
+import com.imcys.bilibilias.logic.login.LoginComponent
+import com.imcys.bilibilias.logic.player.PlayerComponent
 import com.imcys.bilibilias.logic.root.RootComponent.Child.CacheChild
 import com.imcys.bilibilias.logic.root.RootComponent.Child.LoginChild
 import com.imcys.bilibilias.logic.root.RootComponent.Child.PlayerChild
 import com.imcys.bilibilias.logic.root.RootComponent.Child.SearchChild
 import com.imcys.bilibilias.logic.root.RootComponent.Child.SettingsChild
-import com.imcys.bilibilias.logic.search.DefaultSearchComponent
-import com.imcys.bilibilias.logic.setting.DefaultSettingsComponent
+import com.imcys.bilibilias.logic.search.SearchComponent
+import com.imcys.bilibilias.logic.setting.SettingsComponent
 import kotlinx.serialization.Serializable
+import org.koin.core.component.inject
 
 class DefaultRootComponent(
     componentContext: AppComponentContext,
 ) : RootComponent, AppComponentContext by componentContext {
-
+    private val searchComponent by inject<SearchComponent>()
+    private val cacheComponent by inject<CacheComponent>()
+    private val loginComponent by inject<LoginComponent>()
+    private val playerComponent by inject<PlayerComponent>()
+    private val settingsComponent by inject<SettingsComponent>()
     private val nav = StackNavigation<Config>()
 
     private val _stack = childStack(
@@ -36,11 +41,11 @@ class DefaultRootComponent(
 
     private fun child(config: Config, componentContext: AppComponentContext): RootComponent.Child =
         when (config) {
-            Config.Search -> SearchChild(DefaultSearchComponent(componentContext))
-            Config.Cache -> CacheChild(DefaultCacheComponent(componentContext))
-            Config.Login -> LoginChild(DefaultLoginComponent(componentContext))
-            Config.Player -> PlayerChild(DefaultPlayerComponent(componentContext))
-            Config.Settings -> SettingsChild(DefaultSettingsComponent(componentContext))
+            Config.Search -> SearchChild(searchComponent)
+            Config.Cache -> CacheChild(cacheComponent)
+            Config.Login -> LoginChild(loginComponent)
+            Config.Player -> PlayerChild(playerComponent)
+            Config.Settings -> SettingsChild(settingsComponent)
         }
 
     override fun onSettingsClicked() {
