8a6ee5a12b9c0740500eeec0273761e6c6831b36
sock_magic <startxingcheng@gmail.com>
2025-07-31 10:01:41 +0800

    Refactor: Enhance PollResponse with OauthCode status
    This commit introduces an `OauthCode` value class to represent the different states of the OAuth polling process and integrates it into `PollResponse`.
    
    Specific changes:
    - Added `OauthCode` value class in `core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/model/PollRespon.kt`.
        - Defined companion object values for known OAuth codes: `Success`, `WaitingScanned`, `waitingConfirmation`, and `Expired`.
        - Added a `parse` function to convert an integer code into an `OauthCode` instance, throwing an `IllegalArgumentException` for unknown codes.
    - Modified `PollResponse.kt`:
        - Added a new transient property `status` of type `OauthCode`, which is derived by parsing the `code` field.
        - Deprecated the `isSuccess` boolean property, recommending the usage of the new `status` property with `OauthCode` instead.
    
    
    Former-commit-id: 60fed6e3341016722bbca97ae24906ac6bc20d83

--- core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/model/PollRespon.kt
+++ core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/model/PollRespon.kt
@@ -2,6 +2,7 @@ package com.imcys.bilibilias.core.datasource.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class PollResponse(
@@ -16,5 +17,28 @@ data class PollResponse(
     @SerialName("url")
     val url: String = ""
 ) {
+    @Deprecated("use OauthCode")
     val isSuccess: Boolean = code == 0
+
+    @Transient
+    val status: OauthCode = OauthCode.parse(code)
+}
+
+@JvmInline
+value class OauthCode(val code: Int) {
+    companion object {
+        val Success = OauthCode(0)
+        val WaitingScanned = OauthCode(86101)
+        val waitingConfirmation = OauthCode(86090)
+        val Expired = OauthCode(86038)
+        fun parse(code: Int): OauthCode {
+            return when (code) {
+                0 -> Success
+                86101 -> WaitingScanned
+                86090 -> waitingConfirmation
+                86038 -> Expired
+                else -> throw IllegalArgumentException("Unknown oauth code: $code")
+            }
+        }
+    }
 }
