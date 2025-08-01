52c680d3d6413ed794d7efe3e9041ef6abfea44c
sock_magic <startxingcheng@gmail.com>
2025-08-01 16:46:23 +0800

    Refactor: Expose userData flow in AsPreferencesDataSource
    This commit introduces a `userData` flow in `AsPreferencesDataSource` to observe user preferences and adds a placeholder `UserData` class.
    
    Specific changes:
    - Added `val userData = userPreferences.data` to `core/datastore/src/commonMain/kotlin/com/imcys/bilibilias/core/datastore/AsPreferencesDataSource.kt`. This exposes a Flow of `UserPreferences`.
    - Created a new empty class `UserData` in `core/datastore/src/commonMain/kotlin/com/imcys/bilibilias/core/datastore/model/UserData.kt`.
    
    
    Former-commit-id: a170a885bc523d6ddf6d4d3588fe299260ffd592

--- core/datastore/src/commonMain/kotlin/com/imcys/bilibilias/core/datastore/AsPreferencesDataSource.kt
+++ core/datastore/src/commonMain/kotlin/com/imcys/bilibilias/core/datastore/AsPreferencesDataSource.kt
@@ -8,6 +8,7 @@ import kotlin.uuid.ExperimentalUuidApi
 class AsPreferencesDataSource(
     private val userPreferences: DataStore<UserPreferences>,
 ) {
+    val userData = userPreferences.data
     @OptIn(ExperimentalUuidApi::class)
     suspend fun setSelfInfo(info: SelfInfo) {
         userPreferences.updateData {
---
+++ core/datastore/src/commonMain/kotlin/com/imcys/bilibilias/core/datastore/model/UserData.kt
@@ -0,0 +1,4 @@
+package com.imcys.bilibilias.core.datastore.model
+
+class UserData {
+}
