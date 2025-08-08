ef593616dd4dde0f103cf6d2829dda81f9dd3aca
sock_magic <startxingcheng@gmail.com>
2025-08-08 22:56:45 +0800

    Refactor: Remove JVM-specific platform implementation
    This commit removes the `Platform.jvm.kt` file, which contained a JVM-specific implementation of the `Platform` interface.
    
    Specific changes:
    - Deleted `app/src/desktopMain/kotlin/com/imcys/bilibilias/Platform.jvm.kt`.
    - Removed the `JVMPlatform` class and the `getPlatform()` function that returned an instance of it.
    
    
    Former-commit-id: fee4a27df21789a9c86339cc5d8eb90560ad5920

--- app/src/desktopMain/kotlin/com/imcys/bilibilias/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.imcys.bilibilias
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
