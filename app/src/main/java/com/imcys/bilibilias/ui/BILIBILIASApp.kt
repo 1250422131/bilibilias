6aa054769bf28be43c9ad0a633185a34f4cd7fed
萌新杰少 <1250422131@qq.com>
2025-08-10 11:57:11 +0800

    doc: README内容
    Former-commit-id: 8131972c3e7fbf699867766ffbe69616a16d0e9b

--- app/src/main/java/com/imcys/bilibilias/ui/BILIBILIASApp.kt
+++ app/src/main/java/com/imcys/bilibilias/ui/BILIBILIASApp.kt
@@ -246,7 +246,7 @@ fun InstructionsPage(onClickKnowAbout: () -> Unit = {}) {
                     modifier = Modifier
                         .padding(horizontal = 16.dp, vertical = 12.dp),
                     verticalArrangement = Arrangement.spacedBy(12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalAlignment = Alignment.Start,
                 ) {
                     item {
                         Text(
