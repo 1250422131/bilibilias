8581303fb3fbdb2365b69e34f11e1d7d18ad1e55
sock_magic <startxingcheng@gmail.com>
2025-08-18 23:27:29 +0800

    Refactor: Simplify cookie handling in CookiesStorageImpl
    This commit simplifies the cookie handling logic within `CookiesStorageImpl` by directly using cookie key-value pairs instead of parsing and rendering cookie headers.
    
    Specific changes:
    - In `get(requestUrl: Url)`:
        - Changed `parseServerSetCookieHeader(it)` to `Cookie(it.key, it.value)` to directly create `Cookie` objects from the stored key-value pairs.
    - In `addCookie(requestUrl: Url, cookie: Cookie)`:
        - Changed `renderSetCookieHeader(cookie)` to `cookie.value` to store the raw cookie value directly.
    
    
    Former-commit-id: b68a02c4bf60990eaa0ead159f1421ff40dfa324

--- core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/persistent/CookiesStorageImpl.kt
+++ core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/persistent/CookiesStorageImpl.kt
@@ -4,8 +4,6 @@ import com.imcys.bilibilias.core.datastore.CookieJarDataSource
 import io.ktor.client.plugins.cookies.CookiesStorage
 import io.ktor.http.Cookie
 import io.ktor.http.Url
-import io.ktor.http.parseServerSetCookieHeader
-import io.ktor.http.renderSetCookieHeader
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
@@ -14,12 +12,12 @@ internal class CookiesStorageImpl(
 ) : CookiesStorage {
     override suspend fun get(requestUrl: Url): List<Cookie> {
         return cookieJar.cookies.map {
-            it.map { parseServerSetCookieHeader(it) }
+            it.map { Cookie(it.key, it.value) }
         }.first()
     }
 
     override suspend fun addCookie(requestUrl: Url, cookie: Cookie) {
-        cookieJar.add(cookie.name, renderSetCookieHeader(cookie))
+        cookieJar.add(cookie.name, cookie.value)
     }
 
     override fun close() = Unit
