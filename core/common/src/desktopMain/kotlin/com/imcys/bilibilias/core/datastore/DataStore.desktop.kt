6995352f4fb6de268c42be604d20e6e763440575
sock_magic <startxingcheng@gmail.com>
2025-08-22 21:39:57 +0800

    Refactor: Remove unused desktop implementation of resolveDataStoreFile
    This commit removes the desktop-specific implementation of the `resolveDataStoreFile` function.
    
    Specific changes:
    - Deleted file `core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/datastore/DataStore.desktop.kt`. This file contained an empty `TODO` implementation for `resolveDataStoreFile` on the desktop platform.
    
    
    Former-commit-id: 677777dace0bfe425f8f3d205ee8e0dbcb23ffeb

--- core/common/src/desktopMain/kotlin/com/imcys/bilibilias/core/datastore/DataStore.desktop.kt
+++
@@ -1,7 +0,0 @@
-package com.imcys.bilibilias.core.datastore
-
-import com.imcys.bilibilias.core.io.SystemPath
-
-actual fun resolveDataStoreFile(name: String): SystemPath {
-    TODO("Not yet implemented")
-}
