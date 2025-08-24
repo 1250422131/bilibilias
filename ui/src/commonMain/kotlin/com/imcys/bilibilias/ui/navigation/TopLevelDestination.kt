a8a334f871d81e2562bfc26e6880bdd05a375c6c
sock_magic <startxingcheng@gmail.com>
2025-08-24 20:03:48 +0800

    Refactor: Update search icon in TopLevelDestination
    This commit changes the icon used for the SEARCH destination in the `TopLevelDestination` enum.
    
    Specific changes:
    - In `ui/src/commonMain/kotlin/com/imcys/bilibilias/ui/navigation/TopLevelDestination.kt`:
        - Changed `selectedIcon` for `SEARCH` from `Icons.Filled.Search` to `Icons.Filled.Explore`.
        - Changed `unselectedIcon` for `SEARCH` from `Icons.Outlined.Search` to `Icons.Outlined.Explore`.
    
    
    Former-commit-id: aa5f7ed83fcd5978adae62cc1acdf12d5f905b84

--- ui/src/commonMain/kotlin/com/imcys/bilibilias/ui/navigation/TopLevelDestination.kt
+++ ui/src/commonMain/kotlin/com/imcys/bilibilias/ui/navigation/TopLevelDestination.kt
@@ -1,10 +1,10 @@
 package com.imcys.bilibilias.ui.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.filled.FileDownload
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Explore
 import androidx.compose.material.icons.outlined.FileDownload
-import androidx.compose.material.icons.outlined.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class TopLevelDestination(
@@ -13,8 +13,8 @@ enum class TopLevelDestination(
     val iconText: String,
 ) {
     SEARCH(
-        selectedIcon = Icons.Filled.Search,
-        unselectedIcon = Icons.Outlined.Search,
+        selectedIcon = Icons.Filled.Explore,
+        unselectedIcon = Icons.Outlined.Explore,
         iconText = "搜索",
     ),
     CACHE(
