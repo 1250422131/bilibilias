82b376bce9ce42456a4c0cdc8ec852f41755f596
sock_magic <startxingcheng@gmail.com>
2025-08-30 21:11:52 +0800

    Refactor: Remove unused `lifecycleScope` from `AppComponentContext`
    This commit removes the `lifecycleScope` property from the `AppComponentContext` interface and its implementation in `DefaultAppComponentContext` as it was unused.
    
    Specific changes:
    - In `logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/AppComponentContext.kt`:
        - Removed `val lifecycleScope: CoroutineScope` from the `AppComponentContext` interface.
        - Removed the override of `lifecycleScope` in the `DefaultAppComponentContext` class.
    
    
    Former-commit-id: e0e774220af1bd1faacecf59934d771ab6f17ffb

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/AppComponentContext.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/root/AppComponentContext.kt
@@ -6,7 +6,6 @@ import com.arkivanov.decompose.GenericComponentContext
 import com.arkivanov.essenty.backhandler.BackHandlerOwner
 import com.arkivanov.essenty.instancekeeper.InstanceKeeperOwner
 import com.arkivanov.essenty.lifecycle.LifecycleOwner
-import com.arkivanov.essenty.lifecycle.coroutines.coroutineScope
 import com.arkivanov.essenty.statekeeper.StateKeeperOwner
 import com.imcys.bilibilias.core.coroutines.BackgroundScope
 import kotlinx.coroutines.CoroutineScope
@@ -15,9 +14,7 @@ import org.koin.core.component.KoinComponent
 interface AppComponentContext :
     GenericComponentContext<AppComponentContext>,
     BackgroundScope,
-    KoinComponent {
-    val lifecycleScope: CoroutineScope
-}
+    KoinComponent
 
 class DefaultAppComponentContext(
     componentContext: ComponentContext,
@@ -27,7 +24,7 @@ class DefaultAppComponentContext(
     StateKeeperOwner by componentContext,
     InstanceKeeperOwner by componentContext,
     BackHandlerOwner by componentContext {
-    override val lifecycleScope: CoroutineScope = coroutineScope()
+
     override val componentContextFactory: ComponentContextFactory<AppComponentContext> =
         ComponentContextFactory { lifecycle, stateKeeper, instanceKeeper, backHandler ->
             val ctx = componentContext.componentContextFactory(
