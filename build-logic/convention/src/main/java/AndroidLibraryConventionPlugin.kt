1c29fa45652658efb2e2f85e0dc49bf642b26b31
sock_magic <startxingcheng@gmail.com>
2025-09-07 20:51:45 +0800

    Refactor: Remove default test dependencies from AndroidLibraryConventionPlugin
    This commit removes the default test dependencies (junit, androidx-junit, androidx-espresso-core) that were previously added automatically to all Android library modules. Modules should now declare their own specific test dependencies.
    
    Specific changes:
    - In `AndroidLibraryConventionPlugin.kt`:
        - Removed the `dependencies` block that added `testImplementation` and `androidTestImplementation` for common testing libraries.
    
    
    Former-commit-id: 7bd5eb27a4569cb3cea0374ec755fa344e387f68

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -3,8 +3,6 @@ import com.imcys.bilibilias.buildlogic.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import com.imcys.bilibilias.buildlogic.libs
 
 class AndroidLibraryConventionPlugin  : Plugin<Project> {
     override fun apply(target: Project) {
@@ -25,18 +23,6 @@ class AndroidLibraryConventionPlugin  : Plugin<Project> {
                 resourcePrefix =
                     path.split("""\W""".toRegex()).drop(1).distinct().joinToString(separator = "_").lowercase() + "_"
             }
-
-            dependencies{
-                add("testImplementation",libs.findLibrary("junit").get())
-                add("androidTestImplementation",libs.findLibrary("androidx-junit").get())
-                add("androidTestImplementation",libs.findLibrary("androidx-espresso-core").get())
-//                add("androidTestImplementation",libs.findLibrary("androidx-ui-test-junit4").get())
-//                add("debugImplementation",libs.findLibrary("androidx-ui-tooling").get())
-//                add("debugImplementation",libs.findLibrary("androidx-test-manifest").get())
-            }
-
         }
     }
-
-
 }
