dea463e7a87ae0cfcec54eeb78642c75f547524c
sock_magic <startxingcheng@gmail.com>
2025-09-07 09:15:55 +0800

    Refactor: Remove unused image processing code in LoginViewModel
    This commit removes the unused `save()` function from `LoginViewModel`, which contained logic for reading, compressing, and saving an image. This functionality was not being used.
    
    Specific changes:
    - In `logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/login/LoginViewModel.kt`:
        - Removed the `save()` function.
        - Removed unused imports related to `io.github.vinceglb.filekit`.
    
    
    Former-commit-id: b1edecaff1da9f9df2fc7b5a19445eb4bfa46084

--- logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/login/LoginViewModel.kt
+++ logic/src/commonMain/kotlin/com/imcys/bilibilias/logic/login/LoginViewModel.kt
@@ -5,13 +5,6 @@ import com.imcys.bilibilias.core.datasource.api.BilibiliApi
 import com.imcys.bilibilias.core.datastore.AsPreferencesDataSource
 import com.imcys.bilibilias.core.datastore.model.SelfInfo
 import com.imcys.bilibilias.core.logging.logger
-import io.github.vinceglb.filekit.FileKit
-import io.github.vinceglb.filekit.ImageFormat
-import io.github.vinceglb.filekit.PlatformFile
-import io.github.vinceglb.filekit.compressImage
-import io.github.vinceglb.filekit.filesDir
-import io.github.vinceglb.filekit.readBytes
-import io.github.vinceglb.filekit.write
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlin.uuid.Uuid
@@ -24,26 +17,6 @@ class LoginViewModel(
     private val api: BilibiliApi,
     private val applicationScope: CoroutineScope,
 ) : ViewModel() {
-    fun save() {
-        applicationScope.launch {
-            // Read an image file
-            val originalImageFile = PlatformFile("/path/to/original.jpg")
-            val imageBytes = originalImageFile.readBytes()
-
-            // Compress the image
-            val compressedBytes = FileKit.compressImage(
-                bytes = imageBytes,
-                quality = 100,
-                maxWidth = 1024,
-                maxHeight = 1024,
-                imageFormat = ImageFormat.PNG
-            )
-
-            // Save the compressed image
-            val compressedFile = PlatformFile(FileKit.filesDir, "compressed.jpg")
-            compressedFile.write(compressedBytes)
-        }
-    }
 
     override fun onCleared() {
         applicationScope.launch {
