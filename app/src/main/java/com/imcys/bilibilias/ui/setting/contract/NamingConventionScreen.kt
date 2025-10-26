0feb420b2fb1b8c3d72d59bcf6a390cf908273fa
hifter <musejinggai@outlook.com>
2025-10-27 01:16:25 +0800

    feat(ui/setting):为NamingConventionScreen添加了输入法边距和置空失焦后改为默认

--- app/src/main/java/com/imcys/bilibilias/ui/setting/contract/NamingConventionScreen.kt
+++ app/src/main/java/com/imcys/bilibilias/ui/setting/contract/NamingConventionScreen.kt
@@ -1,5 +1,6 @@
 package com.imcys.bilibilias.ui.setting.contract
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
@@ -30,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavKey
 import com.imcys.bilibilias.database.entity.download.FileNamePlaceholder
@@ -110,6 +113,11 @@ fun LazyItemScope.NamingRuleEditor(
                 },
                 label = { Text("视频命名规则") },
                 modifier = Modifier.fillMaxWidth()
+                    .onFocusChanged {
+                        if (!it.isFocused && ruleValue.isEmpty()){
+                            onRestoreDefault()
+                        }
+                    }
             )
             AnimatedVisibility(ruleValue != defaultRule) {
                 Button(
@@ -139,7 +147,8 @@ fun NamingConventionContent(
 
     LazyColumn(
         modifier = modifier
-            .padding(vertical = 5.dp, horizontal = 10.dp),
+            .padding(vertical = 5.dp, horizontal = 10.dp)
+            .imePadding(),
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         item {
